fd6d47bcbb234442b02e03b5f75571a29965a87a
Damian Janus <damian.janus@gmail.com>
2023-05-11 14:19:34 +0200

    fix user replies subpage

--- components/comment-list.tsx
+++ components/comment-list.tsx
@@ -1,42 +1,51 @@
 import CommentListItem from '@/components/comment-list-item';
 
 const CommentList = ({ data, parent }: { data: any, parent?: any }) => {
-  let filtered = data.filter(
-    (x: any) => x.parent_author === parent.author && x.parent_permlink === parent.permlink
-  );
+  if (parent) {
+    let filtered = data.filter(
+      (x: any) => x.parent_author === parent.author && x.parent_permlink === parent.permlink
+    );
 
-  let mutedContent = filtered.filter(
-    (item: any) =>
-      parent &&
-      item.depth === 1 &&
-      item.parent_author === parent.author
-  );
+    let mutedContent = filtered.filter(
+      (item: any) =>
+        parent &&
+        item.depth === 1 &&
+        item.parent_author === parent.author
+    );
 
-  let unmutedContent = filtered.filter((md: any) =>
-    mutedContent.every((fd: any) => fd.post_id !== md.post_id)
-  );
+    let unmutedContent = filtered.filter((md: any) =>
+      mutedContent.every((fd: any) => fd.post_id !== md.post_id)
+    );
 
-  const arr = [...mutedContent, ...unmutedContent];
-  const subReplies = data.filter(
-    (x: any) => x.parent_author !== parent.author && x.parent_permlink !== parent.permlink
-  );
-  const tmp = arr.map((cm: any) => {
-    const srFiltered = []
-    if (cm.replies.length > 0) {
-      for (const rp of cm.replies) {
-        srFiltered.push(...subReplies.filter((sr: any) => sr.url.split("#")[1]?.slice(1) === rp))
+    const arr = [...mutedContent, ...unmutedContent];
+    const subReplies = data.filter(
+      (x: any) => x.parent_author !== parent.author && x.parent_permlink !== parent.permlink
+    );
+    const tmp = arr.map((cm: any) => {
+      const srFiltered = []
+      if (cm.replies.length > 0) {
+        for (const rp of cm.replies) {
+          srFiltered.push(...subReplies.filter((sr: any) => sr.url.split("#")[1]?.slice(1) === rp))
+        }
       }
-    }
-    return {
-      ...cm,
-      fullReplies: srFiltered
-    }
-  })
+      return {
+        ...cm,
+        fullReplies: srFiltered
+      }
+    })
 
+    return (
+      <ul className="p-2">
+        {tmp?.map((comment: any) => (
+          <CommentListItem comment={comment} key={comment.post_id} />
+        ))}
+      </ul>
+    );
+  }
 
   return (
     <ul className="p-2">
-      {tmp?.map((comment: any) => (
+      {data?.map((comment: any) => (
         <CommentListItem comment={comment} key={comment.post_id} />
       ))}
     </ul>
