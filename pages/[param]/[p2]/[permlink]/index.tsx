f929b3953097ab986564f7fd305f7edc5ccb1b9a
Damian Janus <damian.janus@gmail.com>
2023-05-16 16:46:18 +0200

    try improving server response time on ssr post page

--- pages/[param]/[p2]/[permlink]/index.tsx
+++ pages/[param]/[p2]/[permlink]/index.tsx
@@ -13,13 +13,10 @@ import {
 import moment from 'moment';
 
 import { Button } from '@/components/ui/button';
-import UserInfo, { UserHoverCard } from '@/components/user-info';
+import { UserHoverCard } from '@/components/user-info';
 import { getAccounts, getFollowCount, getPost } from '@/lib/hive';
-import { useQuery } from '@tanstack/react-query';
-import { useSiteParams } from '@/components/hooks/use-site-params';
 import { DefaultRenderer } from '@hiveio/content-renderer';
 import { getCommunity, getDiscussion } from '@/lib/bridge';
-import CommentList from '@/components/comment-list';
 import Loading from '@/components/loading';
 import dynamic from 'next/dynamic';
 
@@ -28,71 +25,29 @@ const DynamicComments = dynamic(() => import('@/components/comment-list'), {
   ssr: false
 });
 
-function PostPage({ post_s, followCount_s, account_s, discussion_s, community_s, post_html }: any) {
-  const { username, community, permlink } = useSiteParams();
-  const {
-    isLoading: isLoadingPost,
-    error: errorPost,
-    data: post
-  } = useQuery(['getPost', permlink, post_s], () => getPost(username, String(permlink)), {
-    initialData: post_s,
-    enabled: !!username && !!community && !!permlink
-  });
-  const {
-    isLoading: isLoadingFollows,
-    error: errorFollows,
-    data: follows
-  } = useQuery(['followCountData', username, followCount_s], () => getFollowCount(username), {
-    initialData: followCount_s,
-    enabled: !!username && !!community && !!permlink
-  });
-  const {
-    isLoading: isLoadingAccounts,
-    error: errorAccount,
-    data: account
-  } = useQuery(['accountData', username, account_s], () => getAccounts([username]), {
-    initialData: account_s,
-    enabled: !!username && !!community && !!permlink
-  });
-  const {
-    isLoading: isLoadingDiscussion,
-    error: errorDiscussion,
-    data: discussion
-  } = useQuery(['discussionData', permlink, discussion_s], () => getDiscussion(username, String(permlink)), {
-    initialData:  discussion_s,
-    enabled: !!username && !!community && !!permlink
-  });
-  const {
-    isLoading: isLoadingCommunity,
-    error: errorCommunity,
-    data: communityData
-  } = useQuery(['communityData', community, community_s], () => getCommunity(community || community_s.name), {
-    initialData: community_s,
-    enabled: !!username && !!community && !!permlink
-  });
+const DynamicUserInfo = dynamic(() => import('@/components/user-info'), {
+  loading: () => <Loading />,
+  ssr: false
+})
 
-  const accountMetadata = !isLoadingAccounts && account ? JSON.parse(account[0].posting_json_metadata) : [];
-
+function PostPage({ post_s, followCount_s, account_s, discussion_s, community_s, post_html, account_metadata }: any) {
   return (
     <>
-      {(!isLoadingPost && !isLoadingFollows && !isLoadingAccounts) ||
-      (!isLoadingCommunity && post && follows && account) ||
-      communityData ? (
         <div className="bg-slate-50 py-8">
           <div className="mx-auto my-0 max-w-4xl bg-white px-8 py-4">
-            <h1 className="text-3xl font-bold">{post.title}</h1>
-            {accountMetadata ? (
-              <UserInfo
-                name={accountMetadata.profile?.name}
-                author={post.author}
-                author_reputation={post.author_reputation}
-                community_title={communityData?.title}
-                created={post.created}
-                following={follows.following_count}
-                followers={follows.follower_count}
-                about={accountMetadata.profile?.about}
-                joined={account[0].created}
-                active={account[0].last_vote_time}
+            <h1 className="text-3xl font-bold">{post_s.title}</h1>
+            {account_metadata ? (
+              <DynamicUserInfo
+                name={account_metadata.profile?.name}
+                author={post_s.author}
+                author_reputation={post_s.author_reputation}
+                community_title={community_s?.title}
+                created={post_s.created}
+                following={followCount_s.following_count}
+                followers={followCount_s.follower_count}
+                about={account_metadata.profile?.about}
+                joined={account_s[0].created}
+                active={account_s[0].last_vote_time}
               />
             ) : null}
             <hr />
@@ -105,7 +60,7 @@ function PostPage({ post_s, followCount_s, account_s, discussion_s, community_s,
             />
             <div className="clear-both">
               <ul className="flex gap-2">
-                {post.json_metadata?.tags?.map((tag: string) => (
+                {post_s.json_metadata?.tags?.map((tag: string) => (
                   <li key={tag}>
                     <Button variant="ghost" size="sm">
                       #{tag}
@@ -118,22 +73,22 @@ function PostPage({ post_s, followCount_s, account_s, discussion_s, community_s,
               <div className="my-4 flex justify-between">
                 <div className="flex flex-wrap">
                   <Clock />
-                  <span className="px-1" title={String(moment(parseDate(post.created)))}>
-                    {moment(parseDate(post.created)).fromNow()}
+                  <span className="px-1" title={String(moment(parseDate(post_s.created)))}>
+                    {moment(parseDate(post_s.created)).fromNow()}
                   </span>
                   in
-                  <span className="px-1 font-bold hover:text-red-500">{post.community_title}</span>
+                  <span className="px-1 font-bold hover:text-red-500">{post_s.community_title}</span>
                   by
-                  {accountMetadata ? (
+                  {account_metadata ? (
                     <UserHoverCard
-                      name={accountMetadata.profile?.name}
-                      author={post.author}
-                      author_reputation={post.author_reputation}
-                      following={follows.following_count}
-                      followers={follows.follower_count}
-                      about={accountMetadata.profile?.about}
-                      joined={account[0].created}
-                      active={account[0].last_vote_time}
+                      name={account_metadata.profile?.name}
+                      author={post_s.author}
+                      author_reputation={post_s.author_reputation}
+                      following={followCount_s.following_count}
+                      followers={followCount_s.follower_count}
+                      about={account_metadata.profile?.about}
+                      joined={account_s[0].created}
+                      active={account_s[0].last_vote_time}
                     />
                   ) : null}
                 </div>
@@ -142,7 +97,7 @@ function PostPage({ post_s, followCount_s, account_s, discussion_s, community_s,
                   <span className="text-red-500">Reply</span>
                   <span className="mx-1">|</span>
                   <MessageSquare />
-                  <span className="text-red-500">{post.children}</span>
+                  <span className="text-red-500">{post_s.children}</span>
                 </div>
               </div>
               <div className="my-4 flex justify-between">
@@ -151,8 +106,8 @@ function PostPage({ post_s, followCount_s, account_s, discussion_s, community_s,
                     <ArrowUpCircle />
                     <ArrowDownCircle />
                   </div>
-                  <span className="text-red-500">${post.payout?.toFixed(2)}</span>
-                  <span className="text-red-500">{post.active_votes?.length} votes</span>
+                  <span className="text-red-500">${post_s.payout?.toFixed(2)}</span>
+                  <span className="text-red-500">{post_s.active_votes?.length} votes</span>
                 </div>
                 <div className="flex gap-2">
                   <Facebook />
@@ -164,10 +119,9 @@ function PostPage({ post_s, followCount_s, account_s, discussion_s, community_s,
             </div>
           </div>
           <div className="mx-auto my-0 max-w-4xl px-8 py-4">
-            <DynamicComments data={Object.keys(discussion).map((key) => discussion[key])} parent={post} />
+            <DynamicComments data={Object.keys(discussion_s).map((key) => discussion_s[key])} parent={post_s} />
           </div>
         </div>
-      ) : null}
     </>
   );
 }
@@ -187,6 +141,8 @@ export async function getServerSideProps(context: any) {
     community_s = await getCommunity(community);
   }
 
+  const account_metadata = account_s ? JSON.parse(account_s[0].posting_json_metadata) : [];
+
   const renderer = new DefaultRenderer({
     baseUrl: 'https://hive.blog/',
     breaks: true,
@@ -205,7 +161,7 @@ export async function getServerSideProps(context: any) {
 
   const post_html = renderer.render(post_s.body);
 
-  return { props: { post_s, followCount_s, account_s, discussion_s, community_s, post_html } };
+  return { props: { post_s, followCount_s, account_s, discussion_s, community_s, post_html, account_metadata } };
 }
 
 export default PostPage;
