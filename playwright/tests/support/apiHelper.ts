1a39a6ced2a41539e5bebb39563c218f2808fd67
Adam Kociubiński <akociubinski@syncad.com>
2023-06-06 16:27:44 +0200

    Change order direction in api helper of List proposal request

--- playwright/tests/support/apiHelper.ts
+++ playwright/tests/support/apiHelper.ts
@@ -147,10 +147,10 @@ export class ApiHelper {
   // status: 'all' | 'inactive' | 'active' | 'votable' | 'expired';
   //
   async getListProposalsAPI(
-    start: Array<number | string> = [""],
+    start: Array<number | string> = [],
     limit: number = 30,
     order: string = 'by_total_votes',
-    orderDirection: string = 'descending',
+    orderDirection: string = 'ascending',
     status: string = 'votable'
   ) {
     const url = process.env.REACT_APP_API_ENDPOINT;
