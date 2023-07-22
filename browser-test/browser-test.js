034d0cabc4fc6b575f7f6c704953b54ec5f001a6
Bartłomiej Górnicki <bartlomiej.gornicki@gmail.com>
2023-07-22 23:51:38 +0000

    feat: add functionality to add class on links and target

--- browser-test/browser-test.js
+++ browser-test/browser-test.js
@@ -1,14 +1,15 @@
-import {ClientFunction, Selector} from 'testcafe';
+import { ClientFunction, Selector } from "testcafe";
 
-fixture`Getting Started`
-	.page`./index.html`;
+fixture`Getting Started`.page`./index.html`;
 
 const defaultOptions = {
     baseUrl: "https://hive.blog/",
     breaks: true,
     skipSanitization: false,
     allowInsecureScriptTags: false,
+    addTargetBlankToLinks: true,
     addNofollowToLinks: true,
+    addCssClassToLinks: "hive-class",
     doNotShowImages: false,
     ipfsPrefix: "",
     assetsWidth: 640,
@@ -24,17 +25,30 @@ const renderInBrowser = ClientFunction((options, markup) => {
     return renderer.render(markup);
 });
 
-test('Renders properly simple markup', async t => {
-    const markup = "# H1"
+test("Renders properly simple markup", async (t) => {
+    const markup = "# H1";
 
-    await t.click(Selector('#awaiter'))
-    .expect(renderInBrowser({ ...defaultOptions }, markup)).eql('<h1>H1</h1>\n');
+    await t
+        .click(Selector("#awaiter"))
+        .expect(renderInBrowser({ ...defaultOptions }, markup))
+        .eql("<h1>H1</h1>\n");
 });
 
-test('Does not crash on mixed-img markup', async t => {
+test("Does not crash on mixed-img markup", async (t) => {
     const markup = `<img src="![Sacrifice The Truth Logo.jpg](https://cdn.steemitimages.com/DQmUjNstssuPJpjPDDWfRnw1x2tY6AWWKcajDMGpPLA5iJf/Sacrifice%20The%20Truth%20Logo.jpg)"/>`;
     const expected = `<p><img src="brokenimg.jpg" /></p>\n`;
 
-    await t.click(Selector('#awaiter'))
-    .expect(renderInBrowser({ ...defaultOptions }, markup)).eql(expected);
+    await t
+        .click(Selector("#awaiter"))
+        .expect(renderInBrowser({ ...defaultOptions }, markup))
+        .eql(expected);
+});
+
+test("Renders properly simple link markup with class hive-test", async (t) => {
+    const markup = "[Hive Link](https://hive.io)";
+
+    await t
+        .click(Selector("#awaiter"))
+        .expect(renderInBrowser({ ...defaultOptions }, markup))
+        .eql(`<p><a href="https://hive.io" class="hive-class">Hive Link</a></p>\n`);
 });
