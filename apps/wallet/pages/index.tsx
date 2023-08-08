ad66481e120023e6f23035c98b39fa9e0cf1283e
Krzysztof Kocot <k.kocot0@gmail.com>
2023-08-08 07:08:25 +0000

    Resolve "Create index - Main Page for wallet app"

--- apps/wallet/pages/index.tsx
+++ apps/wallet/pages/index.tsx
@@ -1,29 +1,57 @@
-import { useState } from "react";
-import { Button } from "@hive/ui/components/button";
-
-function Header({ title }: { title: string }) {
-  return <h1>{title ? title : "Default title"}</h1>;
-}
+import { Button } from "@hive/ui";
+import { Icons } from "@hive/ui/components/icons";
+import DialogLogin from "@/wallet/components/dialog-login";
 
 export default function HomePage() {
-  const names = ["Ada Lovelace", "Grace Hopper", "Margaret Hamilton"];
-
-  const [likes, setLikes] = useState(0);
-
-  function handleClick() {
-    setLikes(likes + 1);
-  }
-
   return (
-    <div>
-      <Header title="Develop. Preview. Ship. 🚀" />
-      <ul>
-        {names.map((name) => (
-          <li key={name}>{name}</li>
-        ))}
-      </ul>
-
-      <Button onClick={handleClick}>Like ({likes})</Button>
+    <div className="pt-16 flex flex-col sm:flex-row gap-24 mx-2 sm:gap-0 sm:justify-around">
+      <div className="flex flex-col gap-3 sm:gap-8 sm:mr-4">
+        <div className="font-bold text-lg sm:text-3xl">Hive wallet login</div>
+        <p className="text-sm leading-relaxed sm:max-w-xs">
+          Welcome to Hive&apos;s official Hive blockchain wallet. Use it to
+          transfer Hive tokens, Power up, vote for Hive Witnesses and change
+          your Hive profile details.
+        </p>
+        <DialogLogin>
+          <Button className="rounded-none w-28">Login</Button>
+        </DialogLogin>
+      </div>
+      <div className="mr-6 sm:mt-6 sm:w-full sm:max-w-lg">
+        <div className="bg-white border-solid border-[1px] border-slate-200 h-[260px] w-full drop-shadow">
+          <div className="bg-white border-solid border-[1px] border-slate-200 h-[260px] right-[-10px] top-[-12px] w-full relative drop-shadow">
+            <div className="bg-white justify-center border-solid border-[1px] border-slate-200 flex flex-col pl-4 sm:pr-4 w-full right-[-10px] top-[-12px] relative gap-4 h-[260px]  drop-shadow">
+              <div className="font-bold text-lg sm:text-2xl">Hive tokens</div>
+              <div className="flex gap-3">
+                <Icons.hivetoken />
+                <span className="flex flex-col justify-center">
+                  <span className="font-semibold">Hive</span>
+                  <p className="text-xs font-light text-slate-500">
+                    Liquid platform token
+                  </p>
+                </span>
+              </div>{" "}
+              <div className="flex gap-3">
+                <Icons.hivetokenpower />
+                <span className="flex flex-col justify-center">
+                  <span className="font-semibold">Hive power</span>
+                  <p className="text-xs font-light text-slate-500">
+                    Vesting influence token
+                  </p>
+                </span>
+              </div>{" "}
+              <div className="flex gap-3">
+                <Icons.hbdtoken />
+                <span className="flex flex-col justify-center">
+                  <span className="font-semibold">HBD</span>
+                  <p className="text-xs font-light text-slate-500">
+                    Seeks price stability with USD
+                  </p>
+                </span>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
