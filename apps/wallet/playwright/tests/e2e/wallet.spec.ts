0d6e025263c006c1393e3e25816a62cff7596893
Adam Kociubiński <akociubinski@syncad.com>
2023-08-18 11:05:07 +0200

    add class for the login dialog and test

--- apps/wallet/playwright/tests/e2e/wallet.spec.ts
+++ apps/wallet/playwright/tests/e2e/wallet.spec.ts
@@ -1,5 +1,6 @@
 import { expect, test } from "@playwright/test";
 import { WalletPage } from "../support/pages/walletPage";
+import { LoginToVoteDialog } from "../support/pages/loginToVoteDialog";
 
 test.describe("Wallet page tests", () => {
   let walletPage: WalletPage;
@@ -12,8 +13,22 @@ test.describe("Wallet page tests", () => {
     await walletPage.goToWalletPage();
     await expect(walletPage.page.url()).toMatch(/http:\/\/\w+:4000/); // http://denser:4000 or http://localhost:4000
 
+    await expect(walletPage.hiveTokenLabel).toHaveText('Hive');
     await expect(walletPage.hiveTokensLiquidPlatformTokenLabel).toBeVisible();
+    await expect(walletPage.hivePowerTokenLabel).toHaveText('Hive power');
     await expect(walletPage.hiveTokensVestingInfluenceTokenLabel).toBeVisible();
+    await expect(walletPage.hiveTokenHBDLabel).toHaveText('HBD');
     await expect(walletPage.hiveTokensSeeksPriceStabilityWithUSD).toBeVisible();
+  });
+
+  test("move to the login dialog", async ({ page }) => {
+    const loginDialog = new LoginToVoteDialog(page);
+
+    await walletPage.goToWalletPage();
+    await expect(walletPage.page.url()).toMatch(/http:\/\/\w+:4000/); // http://denser:4000 or http://localhost:4000
+
+    await walletPage.hiveWalletLoginButton.click();
+    await loginDialog.validateLoginToVoteDialogIsVisible();
+    await loginDialog.closeLoginDialog();
   });
 });
