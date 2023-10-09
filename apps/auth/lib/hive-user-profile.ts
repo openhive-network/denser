e530b2bc7961c3e55f7aa179ee88f4d23b7fd6ba
Wojciech Barcik <wbarcik@syncad.com>
2023-10-09 10:00:17 +0200

    Refactor getHiveUserProfile() function

--- apps/auth/lib/hive-user-profile.ts
+++ apps/auth/lib/hive-user-profile.ts
@@ -11,58 +11,47 @@ const logger = getLogger('app');
 
 /**
  * Get Hive user's profile data from posting_json_metadata.
+ *
+ * TODO We can also try to read profile from chainAccount.json_metadata,
+ * when chainAccount.posting_json_metadata doesn't exist.
  */
 export async function getHiveUserProfile(hiveUsername: string): Promise<HiveUserProfile> {
     const hiveUserProfile: HiveUserProfile = {};
-
-    // Add properties to user profile.
+    let chainAccount;
     try {
-        const chainAccount = await getAccount(hiveUsername);
-        logger.debug(`typeof chainAccount: ${typeof chainAccount}`);
-        logger.debug(chainAccount);
+        chainAccount = await getAccount(hiveUsername);
         if (!chainAccount) {
             throw new Error(`gethiveUserProfile error: missing blockchain account "${hiveUsername}"`);
         }
-
-        if (Object.prototype.hasOwnProperty
-            .call(chainAccount, 'posting_json_metadata')) {
+        if (Object.prototype.hasOwnProperty.call(
+                chainAccount, 'posting_json_metadata')) {
             let postingJsonMetadata: any = {};
-            try {
                 postingJsonMetadata = JSON.parse(
                     chainAccount.posting_json_metadata
                 );
-            } catch (e) {
-                // do nothing
-            }
-            if (Object.prototype.hasOwnProperty
-                .call(postingJsonMetadata, 'profile')) {
-
-                if (Object.prototype.hasOwnProperty
-                    .call(postingJsonMetadata.profile, 'name')) {
+            if (Object.prototype.hasOwnProperty.call(
+                    postingJsonMetadata, 'profile')) {
+                if (Object.prototype.hasOwnProperty.call(
+                        postingJsonMetadata.profile, 'name')) {
                     hiveUserProfile.name = postingJsonMetadata.profile.name;
                 }
-
-                if (Object.prototype.hasOwnProperty
-                    .call(postingJsonMetadata.profile, 'profile_image')) {
+                if (Object.prototype.hasOwnProperty.call(
+                        postingJsonMetadata.profile, 'profile_image')) {
                     hiveUserProfile.picture = postingJsonMetadata
                         .profile.profile_image;
                 }
-
-                if (Object.prototype.hasOwnProperty
-                    .call(postingJsonMetadata.profile, 'website')) {
+                if (Object.prototype.hasOwnProperty.call(
+                        postingJsonMetadata.profile, 'website')) {
                     hiveUserProfile.website = postingJsonMetadata
                         .profile.website;
                 }
             }
         }
-
-        // TODO We can also try to read profile from
-        // chainAccount.json_metadata, when
-        // chainAccount.posting_json_metadata doesn't exist.
-
     } catch (error) {
-        logger.error(error);
-        throw error;
+        if (!chainAccount) {
+            logger.error(error);
+            throw error;
+        }
     }
     return hiveUserProfile;
 }
