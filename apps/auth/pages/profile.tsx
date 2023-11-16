928b9700c9728e2046f6b4b3e3d45d9d57f96277
Wojciech Barcik <wbarcik@syncad.com>
2023-11-16 10:19:43 +0100

    Improve rendering on Profile page

--- apps/auth/pages/profile.tsx
+++ apps/auth/pages/profile.tsx
@@ -9,7 +9,7 @@ export default function Profile() {
   return (
     <div className="pt-16 flex flex-col sm:flex-row gap-24 mx-2
         sm:gap-0 sm:justify-around">
-      <div className="flex flex-col gap-3 sm:gap-8 sm:mr-4">
+      {user && user.username && (<div className="flex flex-col gap-3 sm:gap-8 sm:mr-4">
         <h1>Your Hive profile</h1>
         <h2>
           This page uses{' '}
@@ -21,15 +21,11 @@ export default function Profile() {
           </a>{' '}
           and the <a href="/api/user" className="keychainify-checked">/api/user</a> route
         </h2>
-        {user && (
-          <>
-            <p style={{ fontStyle: 'italic' }}>
-              Public data from Hive Blockchain
-            </p>
-            <pre>{JSON.stringify(user, null, 2)}</pre>
-          </>
-        )}
-      </div>
+        <p style={{ fontStyle: 'italic' }}>
+          Public data from Hive Blockchain
+        </p>
+        <pre>{JSON.stringify(user, null, 2)}</pre>
+      </div>)}
     </div>
   )
 }
