a0dc58652c855fc0806797b8f7b762413163912f
Wojciech Barcik <wbarcik@syncad.com>
2023-11-16 10:50:50 +0100

    Show less details on Profile page

--- apps/auth/pages/profile.tsx
+++ apps/auth/pages/profile.tsx
@@ -9,23 +9,14 @@ export default function Profile() {
   return (
     <div className="pt-16 flex flex-col sm:flex-row gap-24 mx-2
         sm:gap-0 sm:justify-around">
-      {user && user.username && (<div className="flex flex-col gap-3 sm:gap-8 sm:mr-4">
-        <h1>Your Hive profile</h1>
-        <h2>
-          This page uses{' '}
-          <a
-            href="https://nextjs.org/docs/basic-features/pages#static-generation-recommended"
-            className="keychainify-checked"
-          >
-            Static Generation (SG)
-          </a>{' '}
-          and the <a href="/api/user" className="keychainify-checked">/api/user</a> route
-        </h2>
-        <p style={{ fontStyle: 'italic' }}>
-          Public data from Hive Blockchain
-        </p>
-        <pre>{JSON.stringify(user, null, 2)}</pre>
-      </div>)}
+      {user && user.username && (
+        <div className="flex flex-col gap-3 sm:gap-8 sm:mr-4">
+          <h1>Your Hive profile</h1>
+          <p>
+            You are logged in as user <strong>{user.username}</strong>.
+          </p>
+        </div>
+      )}
     </div>
   )
 }
