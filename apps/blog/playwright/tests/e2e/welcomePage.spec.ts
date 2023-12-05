d43654348c4cffcb52cf2ee8cf00e95b7f9255b8
Adam Kociubiński <akociubinski@syncad.com>
2023-12-05 12:37:16 +0100

    add tests for links

--- apps/blog/playwright/tests/e2e/welcomePage.spec.ts
+++ apps/blog/playwright/tests/e2e/welcomePage.spec.ts
@@ -48,4 +48,45 @@ test.describe('Welcome page tests', () => {
       await expect(subtitles[subTitleIndex]).toHaveText(expectedSubtitles[subTitleIndex]);
     }
   });
+
+  test('validate additional resources in the welcome page', async ({ page }) => {
+    const faqUrl: string = 'https://hive.blog/faq.html';
+    const whitepaperUrl: string = 'https://hive.io/whitepaper.pdf';
+    const appsBuiltOnHiveUrl: string = 'https://hiveprojects.io/';
+    const blockExplorerUrl: string = 'https://hiveblocks.com/';
+
+    await homePage.goto();
+    await homePage.moveToWelcomePage();
+
+    await expect(welcomePage.faqLink).toHaveAttribute('href', faqUrl);
+    await expect(welcomePage.hiveWhitepaperLink).toHaveAttribute('href', whitepaperUrl);
+    await expect(welcomePage.appsBuiltOnHiveLink).toHaveAttribute('href', appsBuiltOnHiveUrl);
+    await expect(welcomePage.hiveBlockExplorerLink).toHaveAttribute('href', blockExplorerUrl);
+  });
+
+  test('validate links of get to know others subtitle in the welcome page', async ({ page }) => {
+    const openHiveChatUrl: string = 'https://openhive.chat/';
+    const openHiveChatHelpUrl: string = 'https://openhive.chat/channel/help';
+    const hiveDiscordUrl: string = 'https://myhive.li/discord';
+    const hiveTelegramUrl: string = 'https://t.me/hiveblockchain';
+
+    await homePage.goto();
+    await homePage.moveToWelcomePage();
+
+    await expect(welcomePage.openHiveChatLink).toHaveAttribute('href', openHiveChatUrl);
+    await expect(welcomePage.openHiveChatHelpLink).toHaveAttribute('href', openHiveChatHelpUrl);
+    await expect(welcomePage.hiveDiscordLink).toHaveAttribute('href', hiveDiscordUrl);
+    await expect(welcomePage.hiveTelegramLink).toHaveAttribute('href', hiveTelegramUrl);
+  });
+
+  test('validate links of equip yourself subtitle in the welcome page', async ({ page }) => {
+    const hiveKeychainUrl: string = 'https://hive-keychain.com';
+    const hiveSignerUrl: string = 'https://hivesigner.com';
+
+    await homePage.goto();
+    await homePage.moveToWelcomePage();
+
+    await expect(welcomePage.hiveKeychainLink).toHaveAttribute('href', hiveKeychainUrl);
+    await expect(welcomePage.hiveSignerLink).toHaveAttribute('href', hiveSignerUrl);
+  });
 });
