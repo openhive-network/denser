c84908291f3f79ae269ca49b47ec012be7094e30
Adam Kociubiński <akociubinski@syncad.com>
2023-12-07 14:46:41 +0100

    add tests validating styles

--- apps/blog/playwright/tests/e2e/faqPage.spec.ts
+++ apps/blog/playwright/tests/e2e/faqPage.spec.ts
@@ -48,4 +48,109 @@ test.describe('Welcome page tests', () => {
     await expect(faqPage.page).toHaveScreenshot('whatishiveblog.png');
   });
 
+  test('validate styles of faq page titles and links part in the light mode', async ({ page }) => {
+    await homePage.goto();
+    await homePage.moveToFaqPage();
+
+    // Validate main title styles of the FAQ page
+    const faqPageTitleColor = await faqPage.getElementCssPropertyValue(faqPage.mainTitle, "color");
+    expect(await faqPageTitleColor).toBe('rgb(17, 24, 39)');
+    const faqPageTitleFontWeight = await faqPage.getElementCssPropertyValue(faqPage.mainTitle, "font-weight");
+    expect(await faqPageTitleFontWeight).toBe('800');
+    // Validate styles of the first subtopic
+    const faqPageFirstSubTopicOfContentColor = await faqPage.getElementCssPropertyValue(faqPage.firstSubTopicOfContent, "color");
+    expect(await faqPageFirstSubTopicOfContentColor).toBe('rgb(17, 24, 39)');
+    const faqPageFirstSubTopicOfContentFontWeight = await faqPage.getElementCssPropertyValue(faqPage.firstSubTopicOfContent, "font-weight");
+    expect(await faqPageFirstSubTopicOfContentFontWeight).toBe('600');
+    // Validate styles of the first link in the General subtopic
+    const faqPageFirstLinkOfFirstSubtopicColor = await faqPage.getElementCssPropertyValue(faqPage.whatIsHiveBlogLink, "color");
+    expect(await faqPageFirstLinkOfFirstSubtopicColor).toBe('rgb(220, 38, 38)');
+    const faqPageFirstLinkOfFirstSubtopicFontWeight = await faqPage.getElementCssPropertyValue(faqPage.whatIsHiveBlogLink, "font-weight");
+    expect(await faqPageFirstLinkOfFirstSubtopicFontWeight).toBe('400');
+    // Validate styles of the 'Is there a Github page for Hive.blog?'
+    const faqPageIsThereGithubPageForHiveBlogLinkColor = await faqPage.getElementCssPropertyValue(faqPage.isThereGithubPageForHiveBlogLink, "color");
+    expect(await faqPageIsThereGithubPageForHiveBlogLinkColor).toBe('rgb(220, 38, 38)');
+    const faqPageIsThereGithubPageForHiveBlogLinkFontWeight = await faqPage.getElementCssPropertyValue(faqPage.isThereGithubPageForHiveBlogLink, "font-weight");
+    expect(await faqPageIsThereGithubPageForHiveBlogLinkFontWeight).toBe('400');
+  });
+
+  test('validate styles of faq page titles and links part in the dark mode', async ({ page }) => {
+    await homePage.goto();
+    await homePage.moveToFaqPage();
+    // move to the dark mode
+    await homePage.changeThemeMode('Dark');
+    await homePage.validateThemeModeIsDark();
+
+    // Validate main title styles of the FAQ page
+    const faqPageTitleColor = await faqPage.getElementCssPropertyValue(faqPage.mainTitle, "color");
+    expect(await faqPageTitleColor).toBe('rgb(255, 255, 255)');
+    const faqPageTitleFontWeight = await faqPage.getElementCssPropertyValue(faqPage.mainTitle, "font-weight");
+    expect(await faqPageTitleFontWeight).toBe('800');
+    // Validate styles of the first subtopic
+    const faqPageFirstSubTopicOfContentColor = await faqPage.getElementCssPropertyValue(faqPage.firstSubTopicOfContent, "color");
+    expect(await faqPageFirstSubTopicOfContentColor).toBe('rgb(255, 255, 255)');
+    const faqPageFirstSubTopicOfContentFontWeight = await faqPage.getElementCssPropertyValue(faqPage.firstSubTopicOfContent, "font-weight");
+    expect(await faqPageFirstSubTopicOfContentFontWeight).toBe('600');
+    // Validate styles of the first link in the General subtopic
+    const faqPageFirstLinkOfFirstSubtopicColor = await faqPage.getElementCssPropertyValue(faqPage.whatIsHiveBlogLink, "color");
+    expect(await faqPageFirstLinkOfFirstSubtopicColor).toBe('rgb(220, 38, 38)');
+    const faqPageFirstLinkOfFirstSubtopicFontWeight = await faqPage.getElementCssPropertyValue(faqPage.whatIsHiveBlogLink, "font-weight");
+    expect(await faqPageFirstLinkOfFirstSubtopicFontWeight).toBe('400');
+    // Validate styles of the 'Is there a Github page for Hive.blog?'
+    const faqPageIsThereGithubPageForHiveBlogLinkColor = await faqPage.getElementCssPropertyValue(faqPage.isThereGithubPageForHiveBlogLink, "color");
+    expect(await faqPageIsThereGithubPageForHiveBlogLinkColor).toBe('rgb(220, 38, 38)');
+    const faqPageIsThereGithubPageForHiveBlogLinkFontWeight = await faqPage.getElementCssPropertyValue(faqPage.isThereGithubPageForHiveBlogLink, "font-weight");
+    expect(await faqPageIsThereGithubPageForHiveBlogLinkFontWeight).toBe('400');
+  });
+
+  test('validate styles of the elements with content in faq page in the dark mode', async ({ page }) => {
+    await homePage.goto();
+    await homePage.moveToFaqPage();
+    // move to the dark mode
+    await homePage.changeThemeMode('Dark');
+    await homePage.validateThemeModeIsDark();
+
+    // Validate styles of the subtitle of content article in the FAQ page
+    const firstSubTopicsOfContentDescriptionColor = await faqPage.getElementCssPropertyValue(faqPage.firstSubTopicsOfContentDescription, "color");
+    expect(await firstSubTopicsOfContentDescriptionColor).toBe('rgb(255, 255, 255)');
+    const firstSubTopicsOfContentDescriptionFontWeight = await faqPage.getElementCssPropertyValue(faqPage.firstSubTopicsOfContentDescription, "font-weight");
+    expect(await firstSubTopicsOfContentDescriptionFontWeight).toBe('800');
+    // Validate styles of the 'What is hive.blog?' title of content article
+    const whatIsHiveBlogContentHeaderColor = await faqPage.getElementCssPropertyValue(faqPage.whatIsHiveBlogContentHeader, "color");
+    expect(await whatIsHiveBlogContentHeaderColor).toBe('rgb(255, 255, 255)');
+    const whatIsHiveBlogContentHeaderFontWeight = await faqPage.getElementCssPropertyValue(faqPage.whatIsHiveBlogContentHeader, "font-weight");
+    expect(await whatIsHiveBlogContentHeaderFontWeight).toBe('700');
+    // Validate styles of the 'What is hive.blog?' content article (description)
+    const whatIsHiveBlogContentDescriptionColor = await faqPage.getElementCssPropertyValue(faqPage.whatIsHiveBlogContentDescription, "color");
+    expect(await whatIsHiveBlogContentDescriptionColor).toBe('rgb(209, 213, 219)');
+    const whatIsHiveBlogContentDescriptionFontWeight = await faqPage.getElementCssPropertyValue(faqPage.whatIsHiveBlogContentDescription, "font-weight");
+    expect(await whatIsHiveBlogContentDescriptionFontWeight).toBe('400');
+    // Validate styles of the first caret sign
+    const firstCaretSignColor = await faqPage.getElementCssPropertyValue(faqPage.firstCaretSign, "color");
+    expect(await firstCaretSignColor).toBe('rgb(220, 38, 38)');
+  });
+
+  test('validate styles of the elements with content in faq page in the light mode', async ({ page }) => {
+    await homePage.goto();
+    await homePage.moveToFaqPage();
+
+    // Validate styles of the subtitle of content article in the FAQ page
+    const firstSubTopicsOfContentDescriptionColor = await faqPage.getElementCssPropertyValue(faqPage.firstSubTopicsOfContentDescription, "color");
+    expect(await firstSubTopicsOfContentDescriptionColor).toBe('rgb(17, 24, 39)');
+    const firstSubTopicsOfContentDescriptionFontWeight = await faqPage.getElementCssPropertyValue(faqPage.firstSubTopicsOfContentDescription, "font-weight");
+    expect(await firstSubTopicsOfContentDescriptionFontWeight).toBe('800');
+    // Validate styles of the 'What is hive.blog?' title of content article
+    const whatIsHiveBlogContentHeaderColor = await faqPage.getElementCssPropertyValue(faqPage.whatIsHiveBlogContentHeader, "color");
+    expect(await whatIsHiveBlogContentHeaderColor).toBe('rgb(17, 24, 39)');
+    const whatIsHiveBlogContentHeaderFontWeight = await faqPage.getElementCssPropertyValue(faqPage.whatIsHiveBlogContentHeader, "font-weight");
+    expect(await whatIsHiveBlogContentHeaderFontWeight).toBe('700');
+    // Validate styles of the 'What is hive.blog?' content article (description)
+    const whatIsHiveBlogContentDescriptionColor = await faqPage.getElementCssPropertyValue(faqPage.whatIsHiveBlogContentDescription, "color");
+    expect(await whatIsHiveBlogContentDescriptionColor).toBe('rgb(55, 65, 81)');
+    const whatIsHiveBlogContentDescriptionFontWeight = await faqPage.getElementCssPropertyValue(faqPage.whatIsHiveBlogContentDescription, "font-weight");
+    expect(await whatIsHiveBlogContentDescriptionFontWeight).toBe('400');
+    // Validate styles of the first caret sign
+    const firstCaretSignColor = await faqPage.getElementCssPropertyValue(faqPage.firstCaretSign, "color");
+    expect(await firstCaretSignColor).toBe('rgb(220, 38, 38)');
+  });
 });
--- apps/blog/playwright/tests/support/pages/faqPage.ts
+++ apps/blog/playwright/tests/support/pages/faqPage.ts
@@ -3,13 +3,35 @@ import { Locator, Page, expect } from '@playwright/test';
 export class FaqPage {
   readonly page: Page;
   readonly subTopicsOfContent: Locator;
+  readonly firstSubTopicOfContent: Locator;
   readonly subTopicsOfContentDescription: Locator;
+  readonly firstSubTopicsOfContentDescription: Locator;
   readonly whatIsHiveBlogLink: Locator;
+  readonly whatIsHiveBlogContentHeader: Locator;
+  readonly whatIsHiveBlogContentDescription: Locator;
+  readonly mainTitle: Locator;
+  readonly isThereGithubPageForHiveBlogLink: Locator;
+  readonly firstCaretSign: Locator;
 
   constructor(page: Page) {
     this.page = page;
     this.subTopicsOfContent = this.page.locator('[id="articleBody"] h3 span');
+    this.firstSubTopicOfContent = this.subTopicsOfContent.first();
     this.subTopicsOfContentDescription = this.page.locator('[id="articleBody"] ul + h1, [id="articleBody"] p + h1');
+    this.firstSubTopicsOfContentDescription = this.subTopicsOfContentDescription.first();
     this.whatIsHiveBlogLink = this.page.getByRole('link', {name: 'What is Hive.blog?'});
+    this.whatIsHiveBlogContentHeader = this.page.getByRole('heading', {name: 'What is hive.blog?'});
+    this.whatIsHiveBlogContentDescription = this.page.locator('[id="articleBody"] h2 + p').first();
+    this.mainTitle = this.page.locator('[id="articleBody"] h1').first();
+    this.isThereGithubPageForHiveBlogLink = this.page.getByRole('link', {name: 'Is there a Github page for Hive.blog?'});
+    this.firstCaretSign = this.page.locator('[href="#Table_of_Contents_General"]').first();
+  }
+
+  async getElementCssPropertyValue(element: Locator, cssProperty: string) {
+    const propertyValue = await element.evaluate((ele, css) => {
+      return window.getComputedStyle(ele).getPropertyValue(css);
+    }, cssProperty);
+    // return value of element's css property
+    return propertyValue;
   }
 }
