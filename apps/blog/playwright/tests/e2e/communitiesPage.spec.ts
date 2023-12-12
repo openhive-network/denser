56773f41bba383f9eeaa60e128bb89be57761ed7
Adam Kociubiński <akociubinski@syncad.com>
2023-12-12 09:59:01 +0100

    improve test validating response tooltip text

--- apps/blog/playwright/tests/e2e/communitiesPage.spec.ts
+++ apps/blog/playwright/tests/e2e/communitiesPage.spec.ts
@@ -561,7 +561,12 @@ test.describe('Communities page tests', () => {
     const responseNumber = await communitiesPage.getFirstResponses.innerText();
     await communitiesPage.getFirstResponses.hover();
     const responseHoverText = await communitiesPage.postCardResponses.innerText();
-    await expect(responseHoverText).toContain(`${responseNumber} responses. Click to respond`);
+    if (parseInt(responseNumber, 10) > 1)
+      await expect(responseHoverText).toContain(`${responseNumber} responses. Click to respond`);
+    else if (parseInt(responseNumber, 10) == 1)
+      await expect(responseHoverText).toContain(`${responseNumber} response. Click to respond`);
+    else
+      await expect(responseHoverText).toContain(`No response. Click to respond`);
 
     await communitiesPage.getFirstResponses.click();
     await expect(postPage.articleFooter).toBeVisible();
