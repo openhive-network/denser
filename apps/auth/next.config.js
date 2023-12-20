93311ab3197e2c195e97c08c85974103f20f2cee
Wojciech Barcik <wbarcik@syncad.com>
2023-12-20 17:01:19 +0100

    WIP

--- apps/auth/next.config.js
+++ apps/auth/next.config.js
@@ -1,6 +1,7 @@
 const path = require('path')
 const withTM = require('next-transpile-modules')(["@hive/ui"])
 const version = require('./version.json');
+const CopyPlugin = require('copy-webpack-plugin');
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
@@ -24,6 +25,14 @@ const nextConfig = {
         GIT_VERSION: JSON.stringify(version.version)
       })
     );
+
+    // copy hb-auth worker.js
+    config.plugins.push(new CopyPlugin({
+      patterns: [{
+        from: path.join(__dirname, '../../node_modules/@hive/hb-auth/dist/worker.js'),
+        to: path.join(__dirname, 'public/auth/')
+      }]
+    }));
 
     return config;
   }
