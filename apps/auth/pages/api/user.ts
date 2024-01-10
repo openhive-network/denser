130f8b34fe3516adc7a960a0ce6cdb2aeb728cc9
Wojciech Barcik <wbarcik@syncad.com>
2024-01-10 20:33:44 +0100

    Fix lint error

--- apps/auth/pages/api/user.ts
+++ apps/auth/pages/api/user.ts
@@ -1,10 +1,9 @@
-import { NextApiRequest, NextApiResponse } from 'next';
-import { sessionOptions } from 'lib/session';
+import { NextApiHandler } from "next";
 import { getIronSession } from 'iron-session';
+import { sessionOptions } from '@angala/lib/session';
 import { defaultUser } from '@angala/lib/auth/utils';
 import { User } from '@angala/types/common';
 import { IronSessionData } from '@angala/types/common';
-import { NextApiHandler } from "next";
 import { apiHandler } from "@angala/lib/api";
 
 const getUser: NextApiHandler<User> = async (req, res) => {
