3432d131df26e06e6afcd0dd5b0442459ae7331a
Krzysztof Kocot <krzysztofkocot@krzysztofs-macbook-pro.home>
2024-01-18 11:25:38 +0100

    fix login dialog UI and add pl translation

--- packages/smart-signer/components/login-form.tsx
+++ packages/smart-signer/components/login-form.tsx
@@ -1,37 +1,37 @@
 /* eslint-disable @next/next/no-img-element */
 import { useEffect, useState } from 'react';
-import { useForm } from "react-hook-form";
+import { useForm } from 'react-hook-form';
 import { zodResolver } from '@hookform/resolvers/zod';
 import * as z from 'zod';
 import { useTranslation } from 'next-i18next';
-import { Separator } from "@hive/ui/components/separator";
-import { getLogger } from "@hive/ui/lib/logging";
-import { hasCompatibleKeychain } from "@smart-signer/lib/hive-keychain";
-import { username } from "@smart-signer/lib/auth/utils";
-import { LoginTypes } from "@smart-signer/types/common";
-import { validateHivePassword } from "@smart-signer/lib/validate-hive-password";
+import { Separator } from '@hive/ui/components/separator';
+import { getLogger } from '@hive/ui/lib/logging';
+import { hasCompatibleKeychain } from '@smart-signer/lib/hive-keychain';
+import { username } from '@smart-signer/lib/auth/utils';
+import { LoginTypes } from '@smart-signer/types/common';
+import { validateHivePassword } from '@smart-signer/lib/validate-hive-password';
+import { Icons } from '@ui/components/icons';
 
 const ZodLoginTypesEnum = z.nativeEnum(LoginTypes);
 type ZodLoginTypesEnum = z.infer<typeof ZodLoginTypesEnum>;
 
 const passwordField = z.object({
-  password: z.string()
-    .superRefine((val, ctx) => {
-      const result = validateHivePassword(val, (v) => v);
-      if (result) {
-        ctx.addIssue({
-          code: z.ZodIssueCode.custom,
-          message: result,
-          fatal: true,
-        });
-        return z.NEVER;
-      }
-      return true;
-    })
+  password: z.string().superRefine((val, ctx) => {
+    const result = validateHivePassword(val, (v) => v);
+    if (result) {
+      ctx.addIssue({
+        code: z.ZodIssueCode.custom,
+        message: result,
+        fatal: true
+      });
+      return z.NEVER;
+    }
+    return true;
+  })
 });
 
 const passwordHbauthField = z.object({
-  passwordHbauth: z.string().min(1, { message: 'Minimum length 1 character' }),
+  passwordHbauth: z.string().min(1, { message: 'Minimum length 1 character' })
 });
 
 const commonFields = z.object({
@@ -39,23 +39,18 @@ const commonFields = z.object({
   useHbauth: z.boolean(),
   useKeychain: z.boolean(),
   useHiveauth: z.boolean(),
-  remember: z.boolean(),
+  remember: z.boolean()
 });
 
-const commonFieldsWithPassword = commonFields.merge(passwordField)
-const commonFieldsWithPasswordHbauth = commonFields.merge(passwordHbauthField)
+const commonFieldsWithPassword = commonFields.merge(passwordField);
+const commonFieldsWithPasswordHbauth = commonFields.merge(passwordHbauthField);
 
 const loginFormSchema = z.discriminatedUnion('loginType', [
-  z.object({ loginType: z.literal(ZodLoginTypesEnum.enum.password) })
-    .merge(commonFieldsWithPassword),
-  z.object({ loginType: z.literal(ZodLoginTypesEnum.enum.hbauth) })
-    .merge(commonFieldsWithPasswordHbauth),
-  z.object({ loginType: z.literal(ZodLoginTypesEnum.enum.hiveauth) })
-    .merge(commonFields),
-  z.object({ loginType: z.literal(ZodLoginTypesEnum.enum.keychain) })
-    .merge(commonFields),
-  z.object({ loginType: z.literal(ZodLoginTypesEnum.enum.hivesigner) })
-    .merge(commonFields),
+  z.object({ loginType: z.literal(ZodLoginTypesEnum.enum.password) }).merge(commonFieldsWithPassword),
+  z.object({ loginType: z.literal(ZodLoginTypesEnum.enum.hbauth) }).merge(commonFieldsWithPasswordHbauth),
+  z.object({ loginType: z.literal(ZodLoginTypesEnum.enum.hiveauth) }).merge(commonFields),
+  z.object({ loginType: z.literal(ZodLoginTypesEnum.enum.keychain) }).merge(commonFields),
+  z.object({ loginType: z.literal(ZodLoginTypesEnum.enum.hivesigner) }).merge(commonFields)
 ]);
 
 export type LoginFormSchema = z.infer<typeof loginFormSchema>;
@@ -68,13 +63,13 @@ const loginFormDefaultValues = {
   useHbauth: false,
   useHiveauth: false,
   useKeychain: false,
-  username: '',
-}
+  username: ''
+};
 
 export function LoginForm({
   errorMessage = '',
   onSubmit = (data: LoginFormSchema) => {},
-  i18nNamespace = 'smart-signer',
+  i18nNamespace = 'smart-signer'
 }: {
   errorMessage: string;
   onSubmit: (data: LoginFormSchema) => void;
@@ -91,10 +86,17 @@ export function LoginForm({
     setIsKeychainSupported(hasCompatibleKeychain());
   }, []);
 
-  const { register, handleSubmit, formState: { errors }, setValue,
-      getValues, trigger, reset } = useForm<LoginFormSchema>({
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    setValue,
+    getValues,
+    trigger,
+    reset
+  } = useForm<LoginFormSchema>({
     resolver: zodResolver(loginFormSchema),
-    defaultValues: loginFormDefaultValues,
+    defaultValues: loginFormDefaultValues
   });
 
   const onKeychainToggle = (e: React.ChangeEvent<HTMLInputElement>) => {
@@ -163,12 +165,9 @@ export function LoginForm({
   return (
     <div className="flex h-screen flex-col justify-start pt-16 sm:h-fit md:justify-center md:pt-0">
       <div className="mx-auto flex w-[440px] max-w-md flex-col items-center">
-        <h2 className="w-full pb-6 text-3xl text-gray-800">
-          {t('login_form.title_action_login')}
-        </h2>
+        <h2 className="w-full pb-6 text-3xl text-gray-800">{t('login_form.title_action_login')}</h2>
         <form method="post" className="w-full">
-
-          <input type="hidden" {...register("loginType")} />
+          <input type="hidden" {...register('loginType')} />
 
           <div className="relative mb-5">
             <input
@@ -176,17 +175,16 @@ export function LoginForm({
               className="block w-full rounded-lg border border-gray-300 px-3 py-2.5 pl-11 text-sm text-gray-900 focus:border-red-500 focus:outline-none focus:ring-red-500"
               placeholder={t('login_form.username_placeholder')}
               autoComplete="username"
-              {...register("username")}
-              aria-invalid={errors.username ? "true" : "false"}
+              {...register('username')}
+              aria-invalid={errors.username ? 'true' : 'false'}
             />
-            <span className="absolute top-0 h-10 w-10 rounded-bl-lg rounded-tl-lg bg-gray-400 text-gray-600">
-              <div className="flex h-full w-full items-center justify-center">
-                {" "}
-                @
-              </div>
-            </span>
+            <div className="pointer-events-none absolute inset-y-0 left-0 flex items-center pl-3">
+              <Icons.atSign className="h-5 w-5" />
+            </div>
             {errors.username?.message && (
-              <p className="text-red-500 text-sm" role="alert">{t(errors.username.message)}</p>
+              <p className="text-sm text-red-500" role="alert">
+                {t(errors.username.message)}
+              </p>
             )}
           </div>
 
@@ -197,15 +195,17 @@ export function LoginForm({
               placeholder={t('login_form.password_placeholder')}
               autoComplete="current-password"
               disabled={disabledPasword}
-              {...register("password")}
+              {...register('password')}
               data-testid="posting-private-key-input"
             />
             {/* @ts-ignore */}
             {errors.password?.message && (
-              <p className="text-red-500 text-sm" role="alert">{
-                /* @ts-ignore */
-                t(errors.password.message)
-                }</p>
+              <p className="text-sm text-red-500" role="alert">
+                {
+                  /* @ts-ignore */
+                  t(errors.password.message)
+                }
+              </p>
             )}
           </div>
 
@@ -216,32 +216,34 @@ export function LoginForm({
               placeholder={t('login_form.password_hbauth_placeholder')}
               autoComplete="current-password"
               disabled={disabledPaswordHbauth}
-              {...register("passwordHbauth")}
+              {...register('passwordHbauth')}
               data-testid="hbauth-password-input"
             />
             {/* @ts-ignore */}
             {errors.passwordHbauth?.message && (
-              <p className="text-red-500 text-sm" role="alert">{
-                /* @ts-ignore */
-                t(errors.passwordHbauth.message)
-                }</p>
+              <p className="text-sm text-red-500" role="alert">
+                {
+                  /* @ts-ignore */
+                  t(errors.passwordHbauth.message)
+                }
+              </p>
             )}
           </div>
 
           <div className="my-6 flex w-full flex-col">
-
             <div className="flex items-center py-1">
               <input
                 type="checkbox"
+                id="useKeychain"
                 value=""
                 className="h-4 w-4 rounded-lg border border-gray-300 focus:outline-none"
-                {...register("useKeychain")}
+                {...register('useKeychain')}
                 disabled={!isKeychainSupported}
-                onChange={e => onKeychainToggle(e)}
-                />
+                onChange={(e) => onKeychainToggle(e)}
+              />
               <label
                 htmlFor="useKeychain"
-                className="ml-2 flex text-sm font-medium text-gray-900"
+                className="ml-2 flex items-center text-sm font-medium text-gray-900"
               >
                 <img
                   className="mr-1 h-4 w-4"
@@ -254,37 +256,32 @@ export function LoginForm({
 
             <div className="flex items-center py-1">
               <input
+                id="useHiveauth"
                 type="checkbox"
                 value=""
                 className="h-4 w-4 rounded-lg border border-gray-300 focus:outline-none"
-                {...register("useHiveauth")}
-                onChange={e => onHiveauthToggle(e)}
-                />
+                {...register('useHiveauth')}
+                onChange={(e) => onHiveauthToggle(e)}
+              />
               <label
                 htmlFor="useHiveauth"
-                className="ml-2 flex text-sm font-medium text-gray-900"
+                className="ml-2 flex items-center text-sm font-medium text-gray-900"
               >
-                <img
-                  className="mr-1 h-4 w-4"
-                  src="/smart-signer/images/hiveauth.png"
-                  alt="Hiveauth logo"
-                />
+                <img className="mr-1 h-4 w-4" src="/smart-signer/images/hiveauth.png" alt="Hiveauth logo" />
                 {t('login_form.use_hiveauth')}
               </label>
             </div>
 
             <div className="flex items-center py-1">
               <input
+                id="useHbauth"
                 type="checkbox"
                 value=""
                 className="h-4 w-4 rounded-lg border border-gray-300 focus:outline-none"
-                {...register("useHbauth")}
-                onChange={e => onHbauthToggle(e)}
-                />
-              <label
-                htmlFor="useHbauth"
-                className="ml-2 flex text-sm font-medium text-gray-900"
-              >
+                {...register('useHbauth')}
+                onChange={(e) => onHbauthToggle(e)}
+              />
+              <label htmlFor="useHbauth" className="ml-2 flex items-center text-sm font-medium text-gray-900">
                 <img
                   className="mr-1 h-4 w-4"
                   src="/smart-signer/images/hive-blog-twshare.png"
@@ -293,22 +290,18 @@ export function LoginForm({
                 {t('login_form.use_hbauth')}
               </label>
             </div>
-
             <div className="flex items-center py-1">
               <input
+                id="remember"
                 type="checkbox"
                 value=""
                 className=" h-4 w-4 rounded-lg border border-gray-300 focus:outline-none"
-                {...register("remember")}
+                {...register('remember')}
               />
-              <label
-                htmlFor="remember"
-                className="ml-2 text-sm font-medium text-gray-900"
-              >
+              <label htmlFor="remember" className="ml-2 text-sm font-medium text-gray-900">
                 {t('login_form.keep_me_logged_in')}
               </label>
             </div>
-
           </div>
 
           <div className="flex items-center justify-between">
@@ -329,7 +322,9 @@ export function LoginForm({
           </div>
 
           <div>
-            <p className="text-red-500 text-sm" role="alert">{errorMessage || '\u00A0'}</p>
+            <p className="text-sm text-red-500" role="alert">
+              {errorMessage || '\u00A0'}
+            </p>
           </div>
 
           <div className="hiveauth-info">
@@ -347,9 +342,7 @@ export function LoginForm({
 
           <div className="mt-4 flex w-full items-center">
             <Separator orientation="horizontal" className="w-1/3" />
-            <span className="w-1/3 text-center text-sm">
-              {t('login_form.more_login_methods')}
-            </span>
+            <span className="w-1/3 text-center text-sm">{t('login_form.more_login_methods')}</span>
             <Separator orientation="horizontal" className="w-1/3" />
           </div>
           <div className="flex justify-center">
