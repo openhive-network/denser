4caa0a9a47910106f76c6f84e746fc88d5b366c1
Krzysztof Kocot <krzysztofkocot@Krzysztofs-MacBook-Pro.local>
2024-01-25 10:17:19 +0100

    user menu

--- apps/blog/components/site-header.tsx
+++ apps/blog/components/site-header.tsx
@@ -191,7 +191,7 @@ const SiteHeader: FC = () => {
               <TooltipProvider>
                 <Tooltip>
                   <TooltipTrigger data-testid="comment-card-footer-downvote">
-                    <UserMenu user={user}>
+                    <UserMenu user={user} notifications={data?.unread}>
                       <div className="relative inline-flex w-fit cursor-pointer items-center justify-center">
                         {data && data.unread !== 0 ? (
                           <div className="absolute bottom-auto left-auto right-0 top-0 z-10 inline-block -translate-y-1/2 translate-x-2/4 rotate-0 skew-x-0 skew-y-0 scale-x-100 scale-y-100 whitespace-nowrap rounded-full bg-red-600 px-2.5 py-1 text-center align-baseline text-xs font-bold leading-none text-white">
