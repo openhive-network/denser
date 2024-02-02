4b273a50e6e7ac992b94edf9508800aed4eb7613
Wojciech Barcik <wbarcik@syncad.com>
2024-02-02 13:39:35 +0100

    Add test vote button

--- apps/auth/pages/profile.tsx
+++ apps/auth/pages/profile.tsx
@@ -1,9 +1,11 @@
 import { GetServerSideProps } from 'next';
 import { useState, useEffect } from "react";
 import { useUser } from '@smart-signer/lib/auth/use-user';
-import { getLogger } from "@hive/ui/lib/logging";
 import { getTranslations } from '@/auth/lib/get-translations';
+import { Button } from '@hive/ui/components/button';
+import { Signer, vote } from '@smart-signer/lib/signer';
 
+import { getLogger } from '@hive/ui/lib/logging';
 const logger = getLogger('app');
 
 export default function Profile() {
@@ -18,6 +20,34 @@ export default function Profile() {
     redirectIfFound: false,
   });
 
+  const developerAccounts = [
+    'guest4test',
+    'guest4test7',
+    'stirlitz',
+  ];
+
+  const testVote = async () => {
+    if (user && user.isLoggedIn) {
+      const vote: vote = {
+        voter: user.username,
+        author: 'afhsdfg',
+        permlink: 'sdagjvxbn',
+        weight: 10000
+      };
+
+      const signer = new Signer();
+      try {
+        await signer.broadcastTransaction({
+          operation: { vote },
+          loginType: user.loginType,
+          username: user.username
+        });
+      } catch (error) {
+        logger.error(error);
+      }
+    }
+  }
+
   return (
     <div className="pt-16 flex flex-col sm:flex-row gap-24 mx-2
         sm:gap-0 sm:justify-around">
@@ -27,6 +57,11 @@ export default function Profile() {
           <p>
             You are logged in as user <strong>{user.username}</strong>.
           </p>
+        {developerAccounts.includes(user.username) && (
+          <Button onClick={testVote} variant="redHover" size="sm" className="h-10">
+            Test Vote
+          </Button>
+        )}
         </div>
       )}
     </div>
