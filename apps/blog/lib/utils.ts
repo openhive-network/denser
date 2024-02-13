acede1136892661b4780019cec378a04f97df8e9
Krzysztof Kocot <k.kocot0@gmail.com>
2024-02-13 11:07:26 +0000

    Resolve "Add upvote/downvote bar on hover next to mana bar"

--- apps/blog/lib/utils.ts
+++ apps/blog/lib/utils.ts
@@ -7,6 +7,8 @@ import remarkableStripper from '@/blog/lib/remmarkable-stripper';
 import { Vote } from './hive';
 import { Entry, JsonMetadata } from '@ui/lib/bridge';
 import { parseDate2 } from '@hive/ui/lib/parse-date';
+import moment from 'moment';
+import { TFunction } from 'i18next';
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
@@ -291,4 +293,23 @@ export function parseCookie(cookie: string): Record<string, string> {
   });
 
   return kv;
+}
+export function hoursAndMinutes(date: Date, t: TFunction<'common_blog', undefined>) {
+  const today = moment();
+  const cooldownMin = moment(date).diff(today, 'minutes') % 60;
+  const cooldownH = moment(date).diff(today, 'hours');
+
+  return (
+    (cooldownH === 1
+      ? t('global.time.an_hour')
+      : cooldownH > 1
+      ? cooldownH + ' ' + t('global.time.hours')
+      : '') +
+    (cooldownH && cooldownMin ? ' and ' : '') +
+    (cooldownMin === 1
+      ? t('global.time.a_minute')
+      : cooldownMin > 0
+      ? cooldownMin + ' ' + t('global.time.minutes')
+      : '')
+  );
 }
