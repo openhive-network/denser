57e123a6194a5972d0961ff10c9f5d1567c1807d
Krzysztof Kocot <k.kocot0@gmail.com>
2024-02-28 20:19:49 +0100

    small fix

--- apps/blog/components/md-editor.tsx
+++ apps/blog/components/md-editor.tsx
@@ -154,13 +154,6 @@ const MdEditor = () => {
             commands={[...(commands.getCommands() as any), imgBtn(inputRef, textApiRef)]}
           />
         </div>
-        <div>
-          <MDEditor
-            value={value}
-            //@ts-ignore
-            onChange={setValue}
-          />
-        </div>
         {isDrag && (
           <div
             className="absolute bottom-0 left-0 right-0 top-0 h-full w-full bg-red-400 bg-opacity-20 "
