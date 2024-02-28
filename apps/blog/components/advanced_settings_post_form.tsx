061e0d80c3a76931db4abfb9f15cb996a06a0ca1
Krzysztof Kocot <k.kocot0@gmail.com>
2024-02-28 20:19:49 +0100

    show post form after login

---
+++ apps/blog/components/advanced_settings_post_form.tsx
@@ -0,0 +1,46 @@
+import { Button } from '@ui/components/button';
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger
+} from '@ui/components/dialog';
+import { Input } from '@ui/components/input';
+import { Label } from '@ui/components/label';
+import { ReactNode } from 'react';
+
+export function AdvancedSettingsPostForm({ children }: { children: ReactNode }) {
+  return (
+    <Dialog>
+      <DialogTrigger asChild>{children}</DialogTrigger>
+      <DialogContent className="sm:max-w-[425px]">
+        <DialogHeader>
+          <DialogTitle>Advanced settings</DialogTitle>
+          <DialogDescription>
+            Make changes to your profile here. Click save when you're done.
+          </DialogDescription>
+        </DialogHeader>
+        <div className="grid gap-4 py-4">
+          <div className="grid grid-cols-4 items-center gap-4">
+            <Label htmlFor="name" className="text-right">
+              Name
+            </Label>
+            <Input id="name" defaultValue="Pedro Duarte" className="col-span-3" />
+          </div>
+          <div className="grid grid-cols-4 items-center gap-4">
+            <Label htmlFor="username" className="text-right">
+              Username
+            </Label>
+            <Input id="username" defaultValue="@peduarte" className="col-span-3" />
+          </div>
+        </div>
+        <DialogFooter>
+          <Button type="submit">Save changes</Button>
+        </DialogFooter>
+      </DialogContent>
+    </Dialog>
+  );
+}
--- apps/blog/components/post-form.tsx
+++ apps/blog/components/post-form.tsx
@@ -8,28 +8,30 @@ import { zodResolver } from '@hookform/resolvers/zod';
 import * as z from 'zod';
 import { Form, FormControl, FormDescription, FormField, FormItem, FormMessage } from '@ui/components/form';
 import { useForm } from 'react-hook-form';
+import useManabars from './hooks/useManabars';
+import { AdvancedSettingsPostForm } from './advanced_settings_post_form';
 
 const accountFormSchema = z.object({
   title: z.string().min(2, { message: '' }),
-  postArea: z.string().min(2, { message: '' }),
-  unknow1: z.string().min(2, { message: '' }),
-  unknow2: z.string().min(2, { message: '' }),
-  unknow3: z.string().min(2, { message: '' }),
+  postArea: z.string().min(1, { message: '' }),
+  postSummary: z.string().max(140, { message: '' }),
+  tags: z.string(),
+  author: z.string().regex(/^[a-zA-Z1-9]+$/, 'Must contain only letters and numbers'),
   category: z.string({ required_error: '' })
 });
-
 type AccountFormValues = z.infer<typeof accountFormSchema>;
 
-export default function PostForm() {
+export default function PostForm({ username }: { username: string }) {
+  const { manabarsData } = useManabars(username);
   const form = useForm<AccountFormValues>({
     resolver: zodResolver(accountFormSchema),
     defaultValues: {
       title: '',
       postArea: '',
-      unknow1: '',
-      unknow2: '',
-      unknow3: '',
-      category: '1'
+      postSummary: '',
+      tags: '',
+      author: '',
+      category: 'myBlog'
     }
   });
   function onSubmit(data: AccountFormValues) {
@@ -77,11 +79,11 @@ export default function PostForm() {
           />
           <FormField
             control={form.control}
-            name="unknow1"
+            name="postSummary"
             render={({ field }) => (
               <FormItem>
                 <FormControl>
-                  <Input placeholder="IDK1" {...field} />
+                  <Input placeholder="Post summary(for posts & SEO, max 140 chars)" {...field} />
                 </FormControl>
                 <FormMessage />
               </FormItem>
@@ -89,23 +91,11 @@ export default function PostForm() {
           />{' '}
           <FormField
             control={form.control}
-            name="unknow2"
+            name="tags"
             render={({ field }) => (
               <FormItem>
                 <FormControl>
-                  <Input placeholder="IDK2" {...field} />
-                </FormControl>
-                <FormMessage />
-              </FormItem>
-            )}
-          />{' '}
-          <FormField
-            control={form.control}
-            name="unknow3"
-            render={({ field }) => (
-              <FormItem>
-                <FormControl>
-                  <Input placeholder="IDK3" className="w-1/2" {...field} />
+                  <Input placeholder="enter your tags separated by a space" {...field} />
                 </FormControl>
                 <FormMessage />
               </FormItem>
@@ -113,10 +103,32 @@ export default function PostForm() {
           />
           <FormField
             control={form.control}
+            name="author"
+            render={({ field }) => (
+              <FormItem>
+                <FormControl>
+                  <Input placeholder="Author(if different from current account)" {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <div className="flex flex-col gap-2">
+            <span>Post options:</span>
+            <span className="text-xs">Author rewards:{' 50% HBD / 50% HP'}</span>
+            <AdvancedSettingsPostForm>
+              <span className="cursor-pointer text-xs text-red-500">Advanced settings</span>
+            </AdvancedSettingsPostForm>
+          </div>
+          <div className="flex flex-col gap-2">
+            <span>Post options:</span>
+            <span className="text-xs">Author rewards:{' ' + manabarsData?.rc.percentageValue + '%'}</span>
+          </div>
+          <FormField
+            control={form.control}
             name="category"
             render={({ field }) => (
               <FormItem>
-                {' '}
                 <div className="flex flex-wrap items-center gap-4">
                   Posting to:
                   <FormControl>
@@ -127,11 +139,7 @@ export default function PostForm() {
                         </SelectTrigger>
                       </FormControl>
                       <SelectContent>
-                        <SelectItem value="1">My blog1</SelectItem>
-                        <SelectItem value="2">My blog2</SelectItem>
-                        <SelectItem value="3">My blog3</SelectItem>
-                        <SelectItem value="4">My blog4</SelectItem>
-                        <SelectItem value="5">My blog5</SelectItem>
+                        <SelectItem value="myBlog">My blog</SelectItem>
                       </SelectContent>
                     </Select>
                   </FormControl>
