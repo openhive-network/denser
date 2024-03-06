7907257a4ab1084c4ac0a15c15d1bc8610db8eba
Krzysztof Kocot <k.kocot0@gmail.com>
2024-03-06 14:58:15 +0000

    Resolve "Create editor"

--- apps/blog/next.config.js
+++ apps/blog/next.config.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const withTM = require('next-transpile-modules')(['@hive/smart-signer', '@hive/ui', '@hive/transaction']);
 const CopyPlugin = require('copy-webpack-plugin');
+const removeImports = require('next-remove-imports')();
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
@@ -44,4 +45,4 @@ const nextConfig = {
 const withBundleAnalyzer = require('@next/bundle-analyzer')({
   enabled: process.env.ANALYZE === 'true'
 });
-module.exports = withTM(withBundleAnalyzer(nextConfig));
+module.exports = withTM(withBundleAnalyzer(removeImports(nextConfig)));
