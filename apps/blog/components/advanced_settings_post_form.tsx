257e298d72c501a119bde786610f17ab9cf55bb6
Krzysztof Kocot <k.kocot0@gmail.com>
2024-03-20 10:48:34 +0100

    beneficiaries list

--- apps/blog/components/advanced_settings_post_form.tsx
+++ apps/blog/components/advanced_settings_post_form.tsx
@@ -15,18 +15,44 @@ import { Icons } from '@ui/components/icons';
 
 export function AdvancedSettingsPostForm({ children, username }: { children: ReactNode; username: string }) {
   const [rewards, setRewards] = useState('50%');
-  const [maxPayout, setMaxPayout] = useState({ type: '', value: '' });
+  const [maxPayout, setMaxPayout] = useState<string | null>(null);
+  const [items, setItems] = useState<{ percent: number; username: string }[]>([]);
   const [splitRewards, setSplitRewards] = useState(100);
+
+  const handleAddAccount = () => {
+    setItems([...items, { percent: 0, username: '' }]);
+  };
+
+  const handleDeleteItem = (index: number, percent: number) => {
+    setItems((prev) => {
+      const newItems = [...prev];
+      newItems.splice(index, 1);
+      return newItems;
+    });
+
+    setSplitRewards((prev) => prev + percent);
+  };
+
+  const handleChangePercent = (index: number, percentChange: number) => {
+    setItems((prev) => {
+      const newItems = [...prev];
+      newItems[index].percent = percentChange;
+      return newItems;
+    });
+
+    setSplitRewards((prev) => prev - percentChange);
+  };
   function handlerMaxPayout(e: string) {
     switch (e) {
       case 'noLimit':
-        setMaxPayout({ type: e, value: 'max' });
+        setMaxPayout(null);
         break;
       case 'decline':
-        setMaxPayout({ type: e, value: '0' });
+        setMaxPayout('0');
+        setRewards('0%');
         break;
       case 'custom':
-        setMaxPayout({ type: e, value: '' });
+        setMaxPayout('');
         break;
     }
   }
@@ -43,23 +69,25 @@ export function AdvancedSettingsPostForm({ children, username }: { children: Rea
           <div className="flex flex-col gap-3">
             <span className="text-lg font-bold">Maximum Accepted Payout</span>
             <span>HBD value of the maximum payout this post will receive.</span>
-            <Select onValueChange={(e) => handlerMaxPayout(e)} defaultValue="noLimit">
-              <SelectTrigger>
-                <SelectValue />
-              </SelectTrigger>
-              <SelectContent>
-                <SelectItem value="noLimit">No limit</SelectItem>
-                <SelectItem value="decline">Decline Payout</SelectItem>
-                <SelectItem value="custom">Custom value</SelectItem>
-              </SelectContent>
-            </Select>
-            {maxPayout.type === 'custom' && (
-              <Input
-                type="number"
-                value={maxPayout.value}
-                onChange={(e) => setMaxPayout({ type: maxPayout.type, value: e.target.value })}
-              />
-            )}
+            <div className="flex flex-col gap-1">
+              <Select onValueChange={(e) => handlerMaxPayout(e)} defaultValue="noLimit">
+                <SelectTrigger>
+                  <SelectValue />
+                </SelectTrigger>
+                <SelectContent>
+                  <SelectItem value="noLimit">No limit</SelectItem>
+                  <SelectItem value="decline">Decline Payout</SelectItem>
+                  <SelectItem value="custom">Custom value</SelectItem>
+                </SelectContent>
+              </Select>
+              {typeof maxPayout === 'string' && (
+                <Input
+                  type="number"
+                  value={maxPayout ? maxPayout : ''}
+                  onChange={(e) => setMaxPayout(e.target.value)}
+                />
+              )}
+            </div>
           </div>
           <div className="flex flex-col gap-3">
             <span className="text-lg font-bold">Author rewards</span>
@@ -78,36 +106,56 @@ export function AdvancedSettingsPostForm({ children, username }: { children: Rea
               Update
             </Link>
           </div>
-          <div className="flex flex-col gap-3">
-            <span className="text-lg font-bold">Who should receive any rewards?</span>
-            <div className="flex flex-col gap-2">
-              <div className="flex items-center gap-5">
+          <div>
+            <ul className="flex flex-col gap-2">
+              <li className="flex items-center gap-5">
                 <Input value={splitRewards + '%'} disabled className="w-16" />
                 <div className="relative col-span-3">
-                  <Input disabled value={username} className="block w-full px-3 py-2.5 pl-11" />
+                  <Input disabled value={'username'} className="block w-full px-3 py-2.5 pl-11" />
                   <div className="pointer-events-none absolute inset-y-0 left-0 flex items-center pl-3">
                     <Icons.atSign className="h-5 w-5" />
                   </div>
                 </div>
-              </div>
-              <Item />
-            </div>
-            <Button variant="link" className="h-fit w-fit px-0 py-1 text-xs text-red-500">
-              Add account
-            </Button>
+              </li>
+              {items.map((item, index) => (
+                <div className="flex" key={index}>
+                  <Item onChangePercent={(percentChange) => handleChangePercent(index, percentChange)} />
+                  <Button
+                    variant="link"
+                    size="xs"
+                    className="text-red-500"
+                    onClick={() => handleDeleteItem(index, item.percent)}
+                  >
+                    Delete
+                  </Button>
+                </div>
+              ))}
+            </ul>
+
+            {items.length < 8 ? (
+              <Button
+                variant="link"
+                className="h-fit w-fit px-0 py-1 text-xs text-red-500"
+                onClick={handleAddAccount}
+              >
+                Add Account
+              </Button>
+            ) : null}
           </div>
           <div className="flex flex-col gap-3">
             <span className="text-lg font-bold">Post templates</span>
             <span>Manage your post templates, other settings here will also be saved/loaded.</span>
-            <Select>
-              <SelectTrigger>
-                <SelectValue placeholder="Choose a template to load" />
-              </SelectTrigger>
-              <SelectContent>
-                <SelectItem value="1">1</SelectItem>
-              </SelectContent>
-            </Select>
-            <Input placeholder="Name of a new template" />
+            <div className="flex flex-col gap-1">
+              <Select>
+                <SelectTrigger>
+                  <SelectValue placeholder="Choose a template to load" />
+                </SelectTrigger>
+                <SelectContent>
+                  <SelectItem value="1">1</SelectItem>
+                </SelectContent>
+              </Select>
+              <Input placeholder="Name of a new template" />
+            </div>
           </div>
         </div>
         <DialogFooter>
@@ -119,16 +167,38 @@ export function AdvancedSettingsPostForm({ children, username }: { children: Rea
     </Dialog>
   );
 }
-function Item() {
+function Item({ onChangePercent }: { onChangePercent: (percentChange: number) => void }) {
+  const [user, setUser] = useState({ percent: '0', username: '' });
+
   return (
-    <div className="flex items-center gap-5">
-      <Input value={'21' + '%'} className="w-16" />
+    <li className="flex items-center gap-5">
+      <Input
+        value={user.percent}
+        className="w-16"
+        onChange={(e) => {
+          setUser((prev) => {
+            return { ...prev, percent: e.target.value };
+          });
+          const percentChange = parseInt(e.target.value, 10);
+          if (!isNaN(percentChange) && percentChange !== 0) {
+            onChangePercent(percentChange);
+          }
+        }}
+      />
       <div className="relative col-span-3">
-        <Input value={'username'} className="block w-full px-3 py-2.5 pl-11" />
+        <Input
+          value={user.username}
+          className="block w-full px-3 py-2.5 pl-11"
+          onChange={(e) => {
+            setUser((prev) => {
+              return { ...prev, username: e.target.value };
+            });
+          }}
+        />
         <div className="pointer-events-none absolute inset-y-0 left-0 flex items-center pl-3">
           <Icons.atSign className="h-5 w-5" />
         </div>
       </div>
-    </div>
+    </li>
   );
 }
