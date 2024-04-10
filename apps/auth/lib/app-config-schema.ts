b27b43118b86561161d0f76d82687d3ea91d3cb6
Wojciech Barcik <wbarcik@syncad.com>
2024-04-10 14:24:14 +0200

    Move schema to another file

---
+++ apps/auth/lib/app-config-schema.ts
@@ -0,0 +1,16 @@
+import { z } from 'zod';
+
+export const appConfigSchema = z.object({
+    api_endpoint: z.string().url(),
+    app_name: z.string().min(1),
+    images_endpoint: z.string().url(),
+    logging_browser_enabled: z.boolean(),
+    logging_log_level: z.enum(['off', 'fatal', 'error', 'warn', 'info', 'debug', 'trace', 'all']),
+    site_domain: z.string().url(),
+
+    server: z.object({
+        secret_cookie_password: z.string().min(1),
+    })
+});
+
+export type AppConfig = z.infer<typeof appConfigSchema>;
--- apps/auth/lib/app-config-service.ts
+++ apps/auth/lib/app-config-service.ts
@@ -1,21 +1,5 @@
-import { z } from 'zod';
 import config from 'config';
-
-export const appConfigSchema = z.object({
-    api_endpoint: z.string().url(),
-    app_name: z.string().min(1),
-    images_endpoint: z.string().url(),
-    logging_browser_enabled: z.boolean(),
-    logging_log_level: z.enum(['off', 'fatal', 'error', 'warn', 'info', 'debug', 'trace', 'all']),
-    site_domain: z.string().url(),
-
-    server: z.object({
-        secret_cookie_password: z.string().min(1),
-    })
-});
-
-export type AppConfig = z.infer<typeof appConfigSchema>;
-
+import { AppConfig } from '@/auth/lib/app-config-schema';
 export class AppConfigService {
     static appConfig: AppConfig;
 
