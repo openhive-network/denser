797211a89180cb296c94ff5a50365cfa1fd6a0af
Wojciech Barcik <wbarcik@syncad.com>
2024-04-15 09:29:50 +0200

    Fix export default

--- apps/blog/lib/ln2list.ts
+++ apps/blog/lib/ln2list.ts
@@ -1,6 +1,8 @@
-export default (str: string): string[] => {
+export const ln2list = (str: string): string[] => {
   return str
     .split(/(\r\n|\n\r|\r|\n)/g)
     .map((x) => x.trim())
     .filter((x) => x !== "");
 };
+
+export default ln2list;
