3c3fd520691dce236ce230eaca83f7a7fbd684f6
Wojciech Barcik <wbarcik@syncad.com>
2024-04-18 11:44:30 +0200

    Fix error messages in validateWifKey

--- packages/smart-signer/lib/validators/validate-wif-key.ts
+++ packages/smart-signer/lib/validators/validate-wif-key.ts
@@ -5,7 +5,7 @@ export function validateWifKey(
   translateFn: (v: string) => string = (v) => v
 ): string | null {
   if (!password) {
-    return translateFn('chainvalidation_js.password_should_not_be_empty');
+    return translateFn('chainvalidation_js.wif_should_not_be_empty');
   }
   // A Hive generated password is a WIF prefixed with a P.
   // It is possible to login directly with a WIF.
@@ -14,10 +14,10 @@ export function validateWifKey(
   if (!/^5[HJK].{45,}/i.test(wif)) {
     // 51 is the wif length.
     // TODO So why do we check for string length 47 or more?
-    return translateFn('chainvalidation_js.invalid_password_format');
+    return translateFn('chainvalidation_js.invalid_wif_format');
   }
   if (!cryptoUtils.isWif(wif)) {
-    return translateFn('chainvalidation_js.invalid_password_checksum');
+    return translateFn('chainvalidation_js.invalid_wif_checksum');
   }
   return null;
 }
