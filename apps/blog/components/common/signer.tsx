704692192c7a29ce9dd41cc891a69d1843d12f7d
Wojciech Barcik <wbarcik@syncad.com>
2024-04-22 16:32:09 +0200

    Fix SignerProvider.useEffect() and add some logging lines

--- apps/blog/components/common/signer.tsx
+++ apps/blog/components/common/signer.tsx
@@ -3,6 +3,9 @@ import { useSigner } from '@smart-signer/lib/use-signer';
 import { transactionService } from '@transaction/index';
 import { createContext, useContext, ReactNode, useMemo, useState, useEffect } from 'react';
 
+import { getLogger } from '@hive/ui/lib/logging';
+const logger = getLogger('app');
+
 type SignerContextType = {
   signer: ReturnType<typeof getSigner>;
 };
@@ -21,14 +24,16 @@ export const SignerProvider = ({ children }: { children: ReactNode }) => {
   const [signer, setSigner] = useState<ReturnType<typeof getSigner> | null>(null);
   const { signerOptions } = useSigner();
   useEffect(() => {
+    logger.info('Starting SignerProvider.useEffect() to setup Signer');
     (async () => {
       const _getSigner = (await import('@smart-signer/lib/signer/get-signer')).getSigner;
       if (signerOptions.username !== '') {
         setSigner(_getSigner(signerOptions));
         transactionService.setSignerOptions(signerOptions);
       }
-    })();
-  }, [signerOptions]);
+    })().catch(logger.error);
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [signerOptions.username]);
 
   // TODO: Wait for signer to be initialized
   return <SignerContext.Provider value={{ signer: signer! }}>{children}</SignerContext.Provider>;
