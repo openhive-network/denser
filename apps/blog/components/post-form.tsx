a8a9ba451c71c18607ed9338a37b03bf69aca171
Krzysztof Kocot <k.kocot0@gmail.com>
2024-05-15 11:32:41 +0200

    hide broken images on Select Cover Card List

--- apps/blog/components/post-form.tsx
+++ apps/blog/components/post-form.tsx
@@ -16,7 +16,7 @@ import { useForm, useWatch } from 'react-hook-form';
 import useManabars from './hooks/useManabars';
 import { AdvancedSettingsPostForm } from './advanced_settings_post_form';
 import MdEditor from './md-editor';
-import { Dispatch, SetStateAction, useContext, useEffect, useMemo, useRef, useState } from 'react';
+import { Dispatch, SetStateAction, useContext, useEffect, useRef, useState } from 'react';
 import clsx from 'clsx';
 import { useLocalStorage } from 'usehooks-ts';
 import { useTranslation } from 'next-i18next';
@@ -28,14 +28,14 @@ import { Entry, getCommunity, getSubscriptions } from '@transaction/lib/bridge';
 import { useRouter } from 'next/router';
 import { hiveChainService } from '@transaction/lib/hive-chain-service';
 import { TFunction } from 'i18next';
-import { debounce, extractLinks, extractUrlsFromJsonString, extractYouTubeVideoIds } from '../lib/utils';
+import { debounce, extractUrlsFromJsonString, extractYouTubeVideoIds } from '../lib/utils';
 import { Icons } from '@ui/components/icons';
 import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from '@ui/components/tooltip';
 import { DEFAULT_PREFERENCES, Preferences } from '../pages/[param]/settings';
-
 import { getLogger } from '@ui/lib/logging';
 import { cn } from '@ui/lib/utils';
-import { proxifyImageUrl } from '@ui/lib/old-profixy';
+import SelectImageList from './select-image-list';
+
 const logger = getLogger('app');
 
 const MAX_TAGS = 8;
@@ -86,58 +86,13 @@ function validateAltUsernameInput(value: string, t: TFunction<'common_wallet', u
     ? t('submit_page.must_contain_only')
     : null;
 }
-function imagePicker(img: string) {
+export function imagePicker(img: string) {
   const checkImg = img.includes('youtube')
     ? `https://img.youtube.com/vi/${extractYouTubeVideoIds(extractUrlsFromJsonString(img))[0]}/0.jpg`
     : img;
   return checkImg;
 }
 
-const AllImages = ({
-  content,
-  value,
-  onChange,
-  t
-}: {
-  content: string;
-  value: string;
-  onChange: (e: string) => void;
-  t: TFunction<'common_blog', undefined>;
-}) => {
-  const images = useMemo(() => extractLinks(content), [content]);
-  const uniqueImages = Array.from(new Set(images));
-  return uniqueImages.length > 0 ? (
-    <div>
-      <span>{t('submit_page.cover_image')}</span>
-      <div className="flex flex-wrap">
-        {uniqueImages.map((e) => (
-          <div className="group" key={e}>
-            <Button
-              type="button"
-              variant="basic"
-              className="relative flex h-fit w-[62px] items-center overflow-hidden rounded-none bg-transparent p-0 group-hover:h-[80px] group-hover:w-[130px]"
-              onClick={() => onChange(e)}
-            >
-              <img
-                src={proxifyImageUrl(imagePicker(e), true)}
-                alt="cover img"
-                //@ts-expect-error
-                onError={(e) => (e.target.style.display = 'none')}
-                loading="lazy"
-                className={clsx(
-                  'h-[60px] w-[60px] object-cover p-1 contrast-50 ease-out group-hover:h-full  group-hover:w-full group-hover:contrast-100 group-hover:duration-700 group-hover:ease-in-out',
-                  {
-                    'bg-red-700 contrast-100': value === e
-                  }
-                )}
-              />
-            </Button>
-          </div>
-        ))}
-      </div>
-    </div>
-  ) : null;
-};
 export default function PostForm({
   username,
   editMode = false,
@@ -430,7 +385,7 @@ export default function PostForm({
                 </FormItem>
               )}
             />
-            <AllImages content={watchedValues.postArea} value={selectedImg} onChange={setSelectedImg} t={t} />
+            <SelectImageList content={watchedValues.postArea} value={selectedImg} onChange={setSelectedImg} />
             {!editMode ? (
               <div className="flex flex-col gap-2">
                 <span>{t('submit_page.post_options')}</span>
