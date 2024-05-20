1db96e2fd719f169f46d585244f5d3d7e00bc96f
Krzysztof Kocot <k.kocot0@gmail.com>
2024-05-20 11:29:34 +0000

    Resolve "Comment Editor does not show Account status and Rewards"

--- apps/blog/components/post-img.tsx
+++ apps/blog/components/post-img.tsx
@@ -5,7 +5,7 @@ import { extractPictureFromPostBody, extractUrlsFromJsonString, extractYouTubeVi
 import type { Entry } from '@transaction/lib/bridge';
 import clsx from 'clsx';
 
-export default function PostImage({ post }: { post: Entry }) {
+function find_first_img(post: Entry) {
   const pictures_extracted = extractPictureFromPostBody(extractUrlsFromJsonString(post.body));
   const youtube_id = extractYouTubeVideoIds(extractUrlsFromJsonString(post.body));
   const regex_for_peakd = /https:\/\/files\.peakd\.com\/[^\s]+\.jpg/;
@@ -14,65 +14,66 @@ export default function PostImage({ post }: { post: Entry }) {
   const match = post.body.match(regex);
   const regexgif = /<img\s+src="([^"]+)"/;
   const matchgif = post.body.match(regexgif);
-
-  function find_first_img() {
-    if (
-      post.json_metadata.links &&
-      post.json_metadata.links[0] &&
-      customEndsWith(post.json_metadata.links[0].slice(0, post.json_metadata.links[0].length - 1), [
-        'png',
-        'webp',
-        'jpeg',
-        'jpg'
-      ])
-    ) {
-      return proxifyImageUrl(
-        post.json_metadata.links[0].slice(0, post.json_metadata.links[0].length - 1),
-        true
-      );
-    }
-    if (post.original_entry && post.original_entry.json_metadata.images) {
-      return proxifyImageUrl(post.original_entry.json_metadata.images[0], true);
-    }
-    if (post.original_entry && post.original_entry.json_metadata.image) {
-      return proxifyImageUrl(post.original_entry.json_metadata.image[0], true);
-    }
-    if (post.json_metadata.image && post.json_metadata.image[0]) {
-      return proxifyImageUrl(post.json_metadata.image[0], true);
-    }
-    if (match && match[1]) {
-      return proxifyImageUrl(match[1], true);
-    }
-    if (post.json_metadata.images && post.json_metadata.images[0]) {
-      return proxifyImageUrl(post.json_metadata.images[0], true);
-    }
-    if (post.json_metadata.flow?.pictures && post.json_metadata.flow?.pictures[0]) {
-      return proxifyImageUrl(post.json_metadata.flow?.pictures[0].url, true);
-    }
-    if (post.json_metadata?.tags && post.json_metadata?.tags.includes('nsfw')) {
-      return proxifyImageUrl(`https://images.hive.blog/u/${post.author}/avatar/`, true);
-    }
-    if (youtube_id[0]) {
-      return proxifyImageUrl(`https://img.youtube.com/vi/${youtube_id[0]}/0.jpg`, true);
-    }
-    if (pictures_extracted[0]) {
-      return proxifyImageUrl(pictures_extracted[0], true);
-    }
-    if (peakd_img !== null) {
-      return proxifyImageUrl(peakd_img[0], true);
-    }
-    if (matchgif && matchgif[1]) {
-      return proxifyImageUrl(matchgif[1], true);
-    }
-    if (!post.title.includes('RE: ') && post.depth === 0) {
-      return proxifyImageUrl(`https://images.hive.blog/u/${post.author}/avatar/large`, true);
-    }
-    return '';
+  if (
+    post.json_metadata.links &&
+    post.json_metadata.links[0] &&
+    customEndsWith(post.json_metadata.links[0].slice(0, post.json_metadata.links[0].length - 1), [
+      'png',
+      'webp',
+      'jpeg',
+      'jpg'
+    ])
+  ) {
+    return proxifyImageUrl(
+      post.json_metadata.links[0].slice(0, post.json_metadata.links[0].length - 1),
+      true
+    );
   }
+  if (post.original_entry && post.original_entry.json_metadata.images) {
+    return proxifyImageUrl(post.original_entry.json_metadata.images[0], true);
+  }
+  if (post.original_entry && post.original_entry.json_metadata.image) {
+    return proxifyImageUrl(post.original_entry.json_metadata.image[0], true);
+  }
+  if (post.json_metadata.image && post.json_metadata.image[0]) {
+    return proxifyImageUrl(post.json_metadata.image[0], true);
+  }
+  if (match && match[1]) {
+    return proxifyImageUrl(match[1], true);
+  }
+  if (post.json_metadata.images && post.json_metadata.images[0]) {
+    return proxifyImageUrl(post.json_metadata.images[0], true);
+  }
+  if (post.json_metadata.flow?.pictures && post.json_metadata.flow?.pictures[0]) {
+    return proxifyImageUrl(post.json_metadata.flow?.pictures[0].url, true);
+  }
+  if (post.json_metadata?.tags && post.json_metadata?.tags.includes('nsfw')) {
+    return proxifyImageUrl(`https://images.hive.blog/u/${post.author}/avatar/`, true);
+  }
+  if (youtube_id[0]) {
+    return proxifyImageUrl(`https://img.youtube.com/vi/${youtube_id[0]}/0.jpg`, true);
+  }
+  if (pictures_extracted[0]) {
+    return proxifyImageUrl(pictures_extracted[0], true);
+  }
+  if (peakd_img !== null) {
+    return proxifyImageUrl(peakd_img[0], true);
+  }
+  if (matchgif && matchgif[1]) {
+    return proxifyImageUrl(matchgif[1], true);
+  }
+  if (!post.title.includes('RE: ') && post.depth === 0) {
+    return proxifyImageUrl(`https://images.hive.blog/u/${post.author}/avatar/large`, true);
+  }
+  return '';
+}
+
+export default function PostImage({ post }: { post: Entry }) {
+  const cardImage = find_first_img(post);
 
   return (
     <>
-      {find_first_img() ? (
+      {cardImage ? (
         <Link
           href={`/${post.category}/@${post.author}/${post.permlink}`}
           data-testid="post-image"
@@ -81,10 +82,10 @@ export default function PostImage({ post }: { post: Entry }) {
           <div className="relative flex h-[210px] items-center overflow-hidden bg-transparent sm:h-[360px] md:mr-3.5 md:max-h-[80px] md:w-fit md:min-w-[130px] md:max-w-[130px]">
             <picture className="articles__feature-img h-ful w-full">
               <source
-                srcSet={proxifyImageUrl(find_first_img(), '256x512').replace(/ /g, '%20')}
+                srcSet={proxifyImageUrl(cardImage, '256x512').replace(/ /g, '%20')}
                 media="(min-width: 1000px)"
               />
-              <img srcSet={find_first_img()} alt="Post image" loading="lazy" className="w-full" />
+              <img srcSet={cardImage} alt="Post image" loading="lazy" className="w-full" />
             </picture>
           </div>
         </Link>
