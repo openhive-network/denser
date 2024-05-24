441135a2b4081518444884d403eadea03c33df35
Wojciech Barcik <wbarcik@syncad.com>
2024-05-24 18:15:44 +0000

    Resolve "Follow-up from "Resolve "To see the updated status of the application, you need to refresh the page."""

--- apps/blog/components/hooks/use-vote-mutation.ts
+++ apps/blog/components/hooks/use-vote-mutation.ts
@@ -5,7 +5,15 @@ import env from '@beam-australia/react-env';
 import { getLogger } from '@ui/lib/logging';
 const logger = getLogger('app');
 
-
+/**
+ * Makes vote transaction.
+ *
+ * See [TanStack Query
+ * mutations](https://tanstack.com/query/v4/docs/framework/react/guides/mutations)
+ *
+ * @export
+ * @return {*}
+ */
 export function useVoteMutation() {
     const queryClient = useQueryClient();
     const voteMutation = useMutation({
@@ -26,25 +34,33 @@ export function useVoteMutation() {
             //   }
             // }
 
-            try {
-                await transactionService.upVote(
-                    author,
-                    permlink,
-                    weight,
-                    (error) => { throw error; },
-                    true
-                );
-                logger.info('Voted: %o',
-                    { voter, author, permlink, weight });
-            } catch (error) {
-                transactionService.handleError(error);
-                throw error;
-            }
+            await transactionService.upVote(
+                author,
+                permlink,
+                weight,
+                (error) => {
+                    // The method `transactionService.handleError` will
+                    // throw toast message to inform user in UI about
+                    // error. If you haven't better idea how to inform
+                    // user, just pass your error there. Note, that
+                    // error will be swallowed there.
+                    transactionService.handleError(error);
+                    // Throwing the error now is a crucial thing for
+                    // @tanstack/react-query, which should know that
+                    // mutation finished with error.
+                    throw error;
+                },
+                true
+            );
+            logger.info('Voted: %o', { voter, author, permlink, weight });
             return { voter, author, permlink, weight };
         },
         onSuccess: (data) => {
             logger.info('useVoteMutation onSuccess data: %o', data);
             const { voter, author, permlink } = data;
+            // We need to invalidate queries, that can be affected by
+            // mutation. This tells @tanstack/react-query, that it
+            // should refetch data for these queries.
             queryClient.invalidateQueries(
                 { queryKey: ['votes', author, permlink, voter] });
             queryClient.invalidateQueries(
@@ -53,12 +69,9 @@ export function useVoteMutation() {
                 { queryKey: ['postData', author, permlink] });
             queryClient.invalidateQueries(
                 { queryKey: ['entriesInfinite'] });
-        },
-        onError: (error) => {
-            throw error;
         }
     });
-    return { voteMutation };
+    return voteMutation;
 };
 
 
