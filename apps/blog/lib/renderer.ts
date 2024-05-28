f47b920b65149b9424dd256031a9d442d5252d0b
Wojciech Barcik <wbarcik@syncad.com>
2024-05-28 21:00:50 +0200

    Refactor code in apps/blog/lib/renderer.ts

--- apps/blog/lib/renderer.ts
+++ apps/blog/lib/renderer.ts
@@ -3,7 +3,7 @@ import { getDoubleSize, proxifyImageUrl } from '@ui/lib/old-profixy';
 import env from '@beam-australia/react-env';
 import imageUserBlocklist from '@hive/ui/config/lists/image-user-blocklist';
 
-const rendererRegular = new DefaultRenderer({
+const renderDefaultOptions = {
     baseUrl: `${env('SITE_DOMAIN')}/`,
     breaks: true,
     skipSanitization: false,
@@ -27,32 +27,13 @@ const rendererRegular = new DefaultRenderer({
         !url.match(`^(/(?!/)|${env('IMAGES_ENDPOINT')})`) &&
         !url.match(`^(/(?!/)|${env('SITE_DOMAIN')})`) &&
         !url.match(`^(/(?!/)|#)`)
-});
+};
+
+const rendererRegular = new DefaultRenderer(renderDefaultOptions);
 
 const rendererNoImages = new DefaultRenderer({
-    baseUrl: `${env('SITE_DOMAIN')}/`,
-    breaks: true,
-    skipSanitization: false,
-    allowInsecureScriptTags: false,
-    addNofollowToLinks: true,
-    addTargetBlankToLinks: true,
-    cssClassForInternalLinks: '',
-    cssClassForExternalLinks: 'link-external',
+    ...renderDefaultOptions,
     doNotShowImages: true,
-    ipfsPrefix: '',
-    assetsWidth: 640,
-    assetsHeight: 480,
-    imageProxyFn: (url: string) => getDoubleSize(proxifyImageUrl(url, true).replace(/ /g, '%20')),
-    usertagUrlFn: (account: string) => '/@' + account,
-    hashtagUrlFn: (hashtag: string) => '/trending/' + hashtag,
-    isLinkSafeFn: (url: string) =>
-        (!!url.match(`^(/(?!/)|${env('IMAGES_ENDPOINT')})`) &&
-            !!url.match(`^(/(?!/)|${env('SITE_DOMAIN')})`)) ||
-        !!url.match(`^(/(?!/)|#)`),
-    addExternalCssClassToMatchingLinksFn: (url: string) =>
-        !url.match(`^(/(?!/)|${env('IMAGES_ENDPOINT')})`) &&
-        !url.match(`^(/(?!/)|${env('SITE_DOMAIN')})`) &&
-        !url.match(`^(/(?!/)|#)`)
 });
 
 export function getRenderer(
@@ -60,10 +41,8 @@ export function getRenderer(
     doNotShowImages: boolean = false
 ): DefaultRenderer {
     if (doNotShowImages || (!!author && imageUserBlocklist.includes(author))) {
-        console.log('Returning rendererNoImages')
         return rendererNoImages;
     } else {
-        console.log('Returning rendererRegular')
         return rendererRegular;
     }
 }
