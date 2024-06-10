75d1bc3ea78e7a92d80898c3cecb7b4a83e1d095
Krzysztof Kocot <k.kocot0@gmail.com>
2024-06-10 08:16:56 +0000

    Resolve "Add slider to upvote/downvote"

--- apps/blog/components/votes.tsx
+++ apps/blog/components/votes.tsx
@@ -3,7 +3,6 @@ import { useUser } from '@smart-signer/lib/auth/use-user';
 import { Icons } from '@ui/components/icons';
 import DialogLogin from './dialog-login';
 import clsx from 'clsx';
-import type { Entry } from '@transaction/lib/bridge';
 import { useTranslation } from 'next-i18next';
 import { useEffect, useState } from 'react';
 import { useVoteMutation } from '../components/hooks/use-vote-mutation';
@@ -11,15 +10,25 @@ import { useQuery } from '@tanstack/react-query';
 import { CircleSpinner } from 'react-spinners-kit';
 import { getListVotesByCommentVoter } from '@transaction/lib/hive';
 import { getLogger } from '@ui/lib/logging';
+import { Entry } from '@transaction/lib/bridge';
+import { DropdownMenuContent, DropdownMenuTrigger, DropdownMenu } from '@ui/components';
+import { Slider } from '@ui/components/slider';
+import { useLoggedUserContext } from './common/logged-user';
 import { handleError } from '@ui/lib/utils';
+
 const logger = getLogger('app');
+
+const VOTE_WEIGHT_DROPDOWN_THRESHOLD = 1.0 * 1000.0 * 1000.0;
 
 const VotesComponent = ({ post }: { post: Entry }) => {
   const { user } = useUser();
   const { t } = useTranslation('common_blog');
   const [isClient, setIsClient] = useState(false);
   const [clickedVoteButton, setClickedVoteButton] = useState('');
+  const [sliderUpvote, setSliderUpvote] = useState([100]);
+  const [sliderDownvote, setSliderDownvote] = useState([100]);
 
+  const voter = user.username;
   useEffect(() => {
     setIsClient(true);
   }, []);
@@ -32,15 +41,25 @@ const VotesComponent = ({ post }: { post: Entry }) => {
       enabled: !!checkVote || !!clickedVoteButton
     }
   );
+  const { net_vests } = useLoggedUserContext();
+  const enable_slider = net_vests > VOTE_WEIGHT_DROPDOWN_THRESHOLD;
 
   const userVote =
     userVotes?.votes[0] && userVotes?.votes[0].voter === user.username ? userVotes.votes[0] : undefined;
-
   const voteMutation = useVoteMutation();
+  const vote_upvoted = userVote ? userVote.vote_percent > 0 : false;
+  const vote_downvoted = userVote ? userVote.vote_percent < 0 : false;
 
+  useEffect(() => {
+    if (userVote && userVote.vote_percent > 0) {
+      setSliderUpvote([userVote.vote_percent / 100]);
+    }
+    if (userVote && userVote.vote_percent < 0) {
+      setSliderDownvote([-userVote.vote_percent / 100]);
+    }
+  }, [userVotes]);
   const submitVote = async (weight: number) => {
     const { author, permlink } = post;
-    const voter = user.username;
     try {
       await voteMutation.mutateAsync({ voter, author, permlink, weight });
     } catch (error) {
@@ -59,13 +78,44 @@ const VotesComponent = ({ post }: { post: Entry }) => {
                 size={18}
                 color="#dc2626"
               />
-            ) : user && user.isLoggedIn ? (
+            ) : user.isLoggedIn && enable_slider && !vote_upvoted ? (
+              <DropdownMenu>
+                <DropdownMenuTrigger asChild>
+                  <Icons.arrowUpCircle
+                    className={clsx(
+                      'h-[18px] w-[18px] rounded-xl text-red-600 hover:bg-red-600 hover:text-white sm:mr-1',
+                      { 'bg-red-600 text-white': userVote && userVote.vote_percent > 0 }
+                    )}
+                  />
+                </DropdownMenuTrigger>
+                <DropdownMenuContent className="h-16 w-64 p-2">
+                  <div className="flex h-full items-center gap-2">
+                    <Icons.arrowUpCircle
+                      className="h-[24px] w-[24px] cursor-pointer rounded-xl text-red-600 hover:bg-red-600 hover:text-white sm:mr-1"
+                      onClick={() => {
+                        if (voteMutation.isLoading) return;
+                        setClickedVoteButton('up');
+                        submitVote(sliderUpvote[0] * 100);
+                      }}
+                    />
+                    <Slider
+                      defaultValue={sliderUpvote}
+                      value={sliderUpvote}
+                      min={1}
+                      className="w-36"
+                      onValueChange={(e: number[]) => setSliderUpvote(e)}
+                    />
+                    <div className="w-fit">{sliderUpvote}%</div>
+                  </div>
+                </DropdownMenuContent>
+              </DropdownMenu>
+            ) : user.isLoggedIn ? (
               <Icons.arrowUpCircle
                 className={clsx(
                   'h-[18px] w-[18px] rounded-xl text-red-600 hover:bg-red-600 hover:text-white sm:mr-1',
                   { 'bg-red-600 text-white': userVote && userVote.vote_percent > 0 }
                 )}
-                onClick={(e) => {
+                onClick={() => {
                   if (voteMutation.isLoading) return;
                   setClickedVoteButton('up');
                   {
@@ -86,7 +136,7 @@ const VotesComponent = ({ post }: { post: Entry }) => {
           </TooltipTrigger>
           <TooltipContent data-testid="upvote-button-tooltip">
             {userVote && userVote.vote_percent > 0
-              ? userVote.vote_percent === 10000
+              ? userVote.vote_percent === 10000 && !enable_slider
                 ? t('cards.post_card.undo_upvote')
                 : t('cards.post_card.undo_upvote_percent', {
                     votePercent: (userVote.vote_percent / 100).toFixed(2)
@@ -105,13 +155,53 @@ const VotesComponent = ({ post }: { post: Entry }) => {
                 size={18}
                 color="#dc2626"
               />
-            ) : user && user.isLoggedIn ? (
+            ) : user.isLoggedIn && enable_slider && !vote_downvoted ? (
+              <DropdownMenu>
+                <DropdownMenuTrigger asChild>
+                  <Icons.arrowDownCircle
+                    className={clsx(
+                      'h-[18px] w-[18px] rounded-xl text-gray-600 hover:bg-gray-600 hover:text-white sm:mr-1',
+                      { 'bg-gray-600 text-white': userVote && userVote.vote_percent < 0 }
+                    )}
+                  />
+                </DropdownMenuTrigger>
+                <DropdownMenuContent className=" w-64 p-2">
+                  <div className="flex h-full items-center gap-2">
+                    <Icons.arrowDownCircle
+                      className="h-[24px] w-[24px] cursor-pointer rounded-xl text-gray-600 hover:bg-gray-600 hover:text-white sm:mr-1"
+                      onClick={() => {
+                        if (voteMutation.isLoading) return;
+                        setClickedVoteButton('down');
+                        submitVote(-sliderDownvote[0] * 100);
+                      }}
+                    />
+                    <Slider
+                      defaultValue={sliderDownvote}
+                      value={sliderDownvote}
+                      min={1}
+                      className="w-36"
+                      onValueChange={(e: number[]) => setSliderDownvote(e)}
+                    />
+                    <div className="w-fit text-red-600">-{sliderDownvote}%</div>
+                  </div>
+                  <div className="flex flex-col gap-1 pt-2 text-sm">
+                    <p>{t('cards.post_card.downvote_warning')}</p>
+                    <ul>
+                      <li>{t('cards.post_card.reason_1')}</li>
+                      <li>{t('cards.post_card.reason_2')}</li>
+                      <li>{t('cards.post_card.reason_3')}</li>
+                      <li>{t('cards.post_card.reason_4')}</li>
+                    </ul>
+                  </div>
+                </DropdownMenuContent>
+              </DropdownMenu>
+            ) : user.isLoggedIn ? (
               <Icons.arrowDownCircle
                 className={clsx(
                   'h-[18px] w-[18px] rounded-xl text-gray-600 hover:bg-gray-600 hover:text-white sm:mr-1',
                   { 'bg-gray-600 text-white': userVote && userVote.vote_percent < 0 }
                 )}
-                onClick={(e) => {
+                onClick={() => {
                   if (voteMutation.isLoading) return;
                   setClickedVoteButton('down');
                   {
@@ -132,7 +222,7 @@ const VotesComponent = ({ post }: { post: Entry }) => {
           </TooltipTrigger>
           <TooltipContent data-testid="downvote-button-tooltip">
             {userVote && userVote.vote_percent < 0
-              ? userVote.vote_percent === -10000
+              ? userVote.vote_percent === -10000 && !enable_slider
                 ? t('cards.post_card.undo_downvote')
                 : t('cards.post_card.undo_downvote_percent', {
                     votePercent: (-userVote.vote_percent / 100).toFixed(2)
