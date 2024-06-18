c2d1eb5d1c373a1c10a5ef49cfbeb8a5c0812cc3
Wojciech Barcik <wbarcik@syncad.com>
2024-06-18 11:04:51 +0200

    Change login form

--- apps/auth/pages/login.tsx
+++ apps/auth/pages/login.tsx
@@ -1,16 +1,38 @@
+import { useRef } from 'react';
 import { GetServerSideProps } from 'next';
+import { useRouter } from 'next/router';
+import { useUser } from '@smart-signer/lib/auth/use-user';
 import { getTranslations } from '@/auth/lib/get-translations';
 import { loginPageController } from '@smart-signer/lib/login-page-controller';
-import { LoginPanel } from '@smart-signer/components/login-panel';
+import SignInForm, { SignInFormRef } from '@smart-signer/components/auth/form';
+import { KeyType } from '@smart-signer/types/common';
 
 export default function LoginPage() {
+  const router = useRouter();
+  const signInFormRef = useRef<SignInFormRef>(null);
+
+  // Here we just check if user is already logged in and we redirect him
+  // to profile page, if he is.
+  const { user } = useUser({
+    redirectTo: '/',
+    redirectIfFound: true
+  });
+
+  function onComplete() {
+    router.push('/');
+  }
 
   return (
-    <div className="mx-2 flex flex-col pt-10">
-        <LoginPanel
-          authenticateOnBackend={true}
-          strict={true}
+    <div className="flex justify-center">
+      <div className="mt-32 max-w-[380px] rounded-md p-0 sm:mt-auto sm:max-w-[450px] sm:px-0">
+        <SignInForm
+          ref={signInFormRef}
+          preferredKeyTypes={[KeyType.posting]}
+          onComplete={onComplete}
+          // authenticateOnBackend={true}
+          // strict={true}
         />
+      </div>
     </div>
   );
 }
