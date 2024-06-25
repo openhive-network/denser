312a4a4fc079fc59a92ca8004e85d172fdd09b25
Damian Janus <damian.janus@gmail.com>
2024-06-25 14:09:12 +0200

    refactor more react way to travers the DOM

--- apps/blog/components/scroll-to-element.tsx
+++ apps/blog/components/scroll-to-element.tsx
@@ -22,7 +22,7 @@ const ScrollToElement: FC<{ rendererRef: RefObject<HTMLDivElement> }> = ({ rende
       } catch (e) {
         console.error(e);
       } finally {
-        document.getElementById(hash)?.scrollIntoView({ behavior: 'smooth' });
+        rendererRef.current.querySelector(`#${hash}`)?.scrollIntoView({ behavior: 'smooth' });
       }
     };
     handleScroll();
