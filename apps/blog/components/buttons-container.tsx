a733575fd1a1aebac13496ca2c6443a30488d206
Krzysztof Kocot <k.kocot0@gmail.com>
2024-07-03 13:39:31 +0200

    Refactor follow and mute buttons for improved performance and code organization

--- apps/blog/components/buttons-container.tsx
+++ apps/blog/components/buttons-container.tsx
@@ -3,6 +3,12 @@ import FollowButton from './follow-button';
 import MuteButton from './mute-button';
 import { User } from '@smart-signer/types/common';
 import { UseInfiniteQueryResult } from '@tanstack/react-query';
+import { useMuteMutation, useUnmuteMutation } from './hooks/use-mute-mutations';
+import { useFollowMutation, useUnfollowMutation } from './hooks/use-follow-mutations';
+import { Button } from '@hive/ui';
+import DialogLogin from './dialog-login';
+import { useTranslation } from 'next-i18next';
+import { handleError } from '@ui/lib/utils';
 
 const ButtonsContainer = ({
   username,
@@ -28,10 +34,80 @@ const ButtonsContainer = ({
   follow: UseInfiniteQueryResult<IFollow[], unknown>;
   mute: UseInfiniteQueryResult<IFollow[], unknown>;
 }) => {
+  const { t } = useTranslation('common_blog');
+
+  const muteMutation = useMuteMutation();
+  const unmuteMutation = useUnmuteMutation();
+  const followMutation = useFollowMutation();
+  const unfollowMutation = useUnfollowMutation();
+
+  const isMute = Boolean(
+    mute.data?.pages[0].some((f) => f.follower === user.username && f.following === username)
+  );
+  const isFollow = Boolean(
+    follow.data?.pages[0].some(
+      (f: { follower: string; following: string }) => f.follower === user.username && f.following === username
+    )
+  );
+  const handlerMute = async () => {
+    if (!isMute) {
+      try {
+        await muteMutation.mutateAsync({ username });
+      } catch (error) {
+        handleError(error, { method: 'mute', params: { username } });
+      }
+    } else {
+      try {
+        await unmuteMutation.mutateAsync({ username });
+      } catch (error) {
+        handleError(error, { method: 'unmute', params: { username } });
+      }
+    }
+  };
+  const handlerFollow = async () => {
+    if (!isFollow) {
+      try {
+        await followMutation.mutateAsync({ username });
+      } catch (error) {
+        handleError(error, { method: 'follow', params: { username } });
+      }
+    } else {
+      try {
+        await unfollowMutation.mutateAsync({ username });
+      } catch (error) {
+        handleError(error, { method: 'unfollow', params: { username } });
+      }
+    }
+  };
+
+  const loading =
+    mute.isLoading ||
+    mute.isFetching ||
+    muteMutation.isLoading ||
+    unmuteMutation.isLoading ||
+    follow.isLoading ||
+    follow.isFetching ||
+    followMutation.isLoading ||
+    unfollowMutation.isLoading;
   return (
     <>
-      <FollowButton username={username} user={user} variant={variant} list={follow} />
-      {user.isLoggedIn ? <MuteButton username={username} user={user} variant={variant} list={mute} /> : null}
+      {user.isLoggedIn ? (
+        <>
+          <FollowButton loading={loading} variant={variant} isFollow={isFollow} onClick={handlerFollow} />
+          <MuteButton loading={loading} variant={variant} isMute={isMute} onClick={handlerMute} />
+        </>
+      ) : (
+        <DialogLogin>
+          <Button
+            className=" hover:text-red-500"
+            variant={variant}
+            size="sm"
+            data-testid="profile-mute-button"
+          >
+            {t('user_profile.mute_button')}
+          </Button>
+        </DialogLogin>
+      )}
     </>
   );
 };
--- apps/blog/components/mute-button.tsx
+++ apps/blog/components/mute-button.tsx
@@ -1,21 +1,13 @@
 import { Button } from '@hive/ui';
-import DialogLogin from './dialog-login';
 import { useTranslation } from 'next-i18next';
-import { User } from '@smart-signer/types/common';
-import { IFollow } from '@transaction/lib/hive';
-import { UseInfiniteQueryResult } from '@tanstack/react-query';
-import { useMuteMutation, useUnmuteMutation } from './hooks/use-mute-mutations';
-import { handleError } from '@ui/lib/utils';
 import { CircleSpinner } from 'react-spinners-kit';
 
 const MuteButton = ({
-  username,
-  user,
   variant,
-  list
+  loading,
+  isMute,
+  onClick
 }: {
-  username: string;
-  user: User;
   variant:
     | 'default'
     | 'destructive'
@@ -28,67 +20,31 @@ const MuteButton = ({
     | 'basic'
     | null
     | undefined;
-  list: UseInfiniteQueryResult<IFollow[], unknown>;
+  loading: boolean;
+  isMute: boolean;
+  onClick: () => void;
 }) => {
   const { t } = useTranslation('common_blog');
-  const muteMutation = useMuteMutation();
-  const unmuteMutation = useUnmuteMutation();
 
-  const isMute = Boolean(
-    list.data?.pages[0].some((f) => f.follower === user.username && f.following === username)
-  );
   return (
-    <>
-      {user.isLoggedIn ? (
-        <Button
-          className=" hover:text-red-500"
-          variant={variant}
-          size="sm"
-          data-testid="profile-mute-button"
-          onClick={async () => {
-            if (!isMute) {
-              try {
-                await muteMutation.mutateAsync({ username });
-              } catch (error) {
-                handleError(error, { method: 'mute', params: { username } });
-              }
-            } else {
-              try {
-                await unmuteMutation.mutateAsync({ username });
-              } catch (error) {
-                handleError(error, { method: 'unmute', params: { username } });
-              }
-            }
-          }}
-          disabled={list.isLoading || list.isFetching || muteMutation.isLoading || unmuteMutation.isLoading}
-        >
-          {muteMutation.isLoading || unmuteMutation.isLoading ? (
-            <span className="flex h-5 w-12 items-center justify-center">
-              <CircleSpinner
-                loading={muteMutation.isLoading || unmuteMutation.isLoading}
-                size={18}
-                color="#dc2626"
-              />
-            </span>
-          ) : isMute ? (
-            t('user_profile.unmute_button')
-          ) : (
-            t('user_profile.mute_button')
-          )}
-        </Button>
+    <Button
+      className=" hover:text-red-500"
+      variant={variant}
+      size="sm"
+      data-testid="profile-mute-button"
+      onClick={() => onClick()}
+      disabled={loading}
+    >
+      {loading ? (
+        <span className="flex h-5 w-12 items-center justify-center">
+          <CircleSpinner loading={loading} size={18} color="#dc2626" />
+        </span>
+      ) : isMute ? (
+        t('user_profile.unmute_button')
       ) : (
-        <DialogLogin>
-          <Button
-            className=" hover:text-red-500"
-            variant={variant}
-            size="sm"
-            data-testid="profile-mute-button"
-          >
-            {t('user_profile.mute_button')}
-          </Button>
-        </DialogLogin>
+        t('user_profile.mute_button')
       )}
-    </>
+    </Button>
   );
 };
 export default MuteButton;
