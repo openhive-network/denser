2a749c42ec7910b5226dda84440f130b0f9bd301
Efe <hbehar@syncad.com>
2024-07-17 09:56:59 +0200

    move package

--- scripts/update-wax.sh
+++ scripts/update-wax.sh
@@ -8,29 +8,17 @@ HBAUTH_VERSION=${2} # full package name, e.g. @hiveio/hb-auth@0.0.1-stable.24032
 uninstall_packages() {
   echo "Uninstalling packages"
 
-  npm uninstall --save @hiveio/hb-auth --workspace=packages/smart-signer
+  pnpm remove --save @hiveio/hb-auth --workspace=packages/smart-signer
 
-  npm uninstall --save @hiveio/wax --workspace=packages/smart-signer
-  npm uninstall --save @hiveio/wax --workspace=packages/transaction
-  npm uninstall --save @hiveio/wax --workspace=packages/ui
-
-  npm uninstall --save @hiveio/wax --workspace=apps/auth
-  npm uninstall --save @hiveio/wax --workspace=apps/blog
-  npm uninstall --save @hiveio/wax --workspace=apps/wallet
+  pnpm remove --save @hiveio/wax --workspace=packages/smart-signer
 }
 
 install_packages() {
   echo "Installing packages"
 
-  npm install --save "${WAX_VERSION}" --workspace=apps/auth
-  npm install --save "${WAX_VERSION}" --workspace=apps/blog
-  npm install --save "${WAX_VERSION}" --workspace=apps/wallet
+  pnpm update --save "${WAX_VERSION}" --workspace=apps/auth
 
-  npm install --save "${WAX_VERSION}" --workspace=packages/smart-signer
-  npm install --save "${WAX_VERSION}" --workspace=packages/transaction
-  npm install --save "${WAX_VERSION}" --workspace=packages/ui
-
-  npm install --save "${HBAUTH_VERSION}" --workspace=packages/smart-signer
+  pnpm update --save "${HBAUTH_VERSION}" --workspace=packages/smart-signer
 }
 
 uninstall_packages
