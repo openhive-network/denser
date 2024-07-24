27a269794f51d6609d792305b597207dcacc8247
Adam Kociubiński <akociubinski@syncad.com>
2024-07-24 11:01:17 +0200

    increase the time waiting for hover

--- apps/blog/playwright/tests/e2e/loginSignUp.spec.ts
+++ apps/blog/playwright/tests/e2e/loginSignUp.spec.ts
@@ -51,15 +51,14 @@ test.describe('Login and Sign Up tests', () =>{
     // expect(await homePage.getElementCssPropertyValue(homePage.loginBtn, 'color')).toBe("rgb(52, 60, 77)");
     await expect(homePage.loginBtn).toHaveText("Login")
     await homePage.loginBtn.hover()
-    await page.waitForTimeout(500)
+    await page.waitForTimeout(1000)
     expect(await homePage.getElementCssPropertyValue(homePage.loginBtn, 'color')).toBe("rgb(226, 18, 53)");
-
 
     await expect(homePage.signupBtn).toBeVisible()
     expect(await homePage.getElementCssPropertyValue(homePage.signupBtn, 'color')).toBe("rgb(255, 255, 255)");
     await expect(homePage.signupBtn).toHaveText("Sign up")
     await homePage.signupBtn.hover()
-    await page.waitForTimeout(500)
+    await page.waitForTimeout(1000)
     expect(await homePage.getElementCssPropertyValue(homePage.signupBtn, 'background-color')).toBe("rgb(220, 38, 38)");
     await homePage.changeThemeMode("Light")
   });
