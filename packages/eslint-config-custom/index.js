1dd13ceee5f13aa894653cbc2bcefe1e8ede0d7e
Efe <hbehar@syncad.com>
2024-07-25 11:25:22 +0200

    update eslint

--- packages/eslint-config-custom/index.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  extends: ['next', 'turbo', 'prettier', 'next/core-web-vitals'],
-  rules: {
-    '@next/next/no-html-link-for-pages': 'off',
-    'turbo/no-undeclared-env-vars': 'off'
-  }
-};
