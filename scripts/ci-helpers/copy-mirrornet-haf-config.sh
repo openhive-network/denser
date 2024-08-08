3f2491339a35528c8ce6776480ce52db4b7b0151
Konrad Botor <kbotor@syncad.com>
2024-08-08 13:21:04 +0200

    Update 31

--- scripts/ci-helpers/copy-mirrornet-haf-config.sh
+++ scripts/ci-helpers/copy-mirrornet-haf-config.sh
@@ -12,7 +12,6 @@ then
     echo "Copying alternate-chain-spec.json file..." 
     cp "${BLOCK_LOG_SOURCE_DIR:?}/alternate-chain-spec.json" "${REPLAY_DIRECTORY:?}/blockchain/alternate-chain-spec.json"
     chown 1000:100 "${REPLAY_DIRECTORY:?}/blockchain/alternate-chain-spec.json"
-    echo "" >> "${REPLAY_DIRECTORY:?}/blockchain/alternate-chain-spec.json"
 fi
 
 echo "Inspecting replay directory..."
