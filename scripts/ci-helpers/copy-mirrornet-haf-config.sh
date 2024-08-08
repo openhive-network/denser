d10bc8669114128c6266583fdea523fcaca2d744
Konrad Botor <kbotor@syncad.com>
2024-08-08 10:48:38 +0200

    Update 24

--- scripts/ci-helpers/copy-mirrornet-haf-config.sh
+++ scripts/ci-helpers/copy-mirrornet-haf-config.sh
@@ -3,4 +3,12 @@
 set -e
 
 echo "Performing additional configuration..."
+
+echo "Copying config.ini file..."
 cp "${CI_PROJECT_DIR:?}/stack/mirrornet_haf_config.ini" "${REPLAY_DIRECTORY:?}/config.ini"
+
+if [[ -e "${BLOCK_LOG_SOURCE_DIR:?}/alternate-chain-spec.json" ]]
+then
+    echo "Copying alternate-chain-spec.json file..." 
+    cp "${BLOCK_LOG_SOURCE_DIR:?}/alternate-chain-spec.json" "${REPLAY_DIRECTORY:?}/blockchain/alternate-chain-spec.json"
+fi
