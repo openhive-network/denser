62b080a9b2590848cac396abb24a016151aa18b1
Wojciech Barcik <wbarcik@syncad.com>
2024-08-11 17:55:46 +0200

    Add docstring

--- packages/smart-signer/lib/api-handlers/auth/chat-token.ts
+++ packages/smart-signer/lib/api-handlers/auth/chat-token.ts
@@ -2,23 +2,22 @@ import createHttpError from 'http-errors';
 import { NextApiHandler } from 'next';
 import { getIronSession } from 'iron-session';
 import { sessionOptions } from '@smart-signer/lib/session';
-import { getAccount } from '@transaction/lib/hive';
-import { postLoginSchema, PostLoginSchema } from '@smart-signer/lib/auth/utils';
 import { User } from '@smart-signer/types/common';
 import { IronSessionData } from '@smart-signer/types/common';
-import { cookieNamePrefix } from '@smart-signer/lib/session';
 import { checkCsrfHeader } from '@smart-signer/lib/csrf-protection';
-import { verifyLoginChallenge } from '@smart-signer/lib/verify-login-challenge';
-import { verifyLogin } from '@smart-signer/lib/verify-login';
-import { getLoginChallengeFromTransactionForLogin } from '@smart-signer/lib/login-operation'
 import { getLogger } from '@hive/ui/lib/logging';
 import { siteConfig } from '@hive/ui/config/site';
 import { getChatAuthToken } from '@smart-signer/lib/rocket-chat';
 
 const logger = getLogger('app');
 
-
-export const getChatToken: NextApiHandler<User> = async (req, res) => {
+/**
+ * Create a new auth token in Rocket Chat and return it in User object.
+ *
+ * @param {*} req
+ * @param {*} res
+ */
+const getChatToken: NextApiHandler<User> = async (req, res) => {
   checkCsrfHeader(req);
 
   let user: User | undefined;
