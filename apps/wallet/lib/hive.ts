61d524ca71b573189449cf2e3a64e3da6fc0e866
Piotr Berezka <berezkapiotr@gmail.com>
2024-08-20 12:18:09 +0200

    add and use find savings withdrawals operation

--- apps/wallet/lib/hive.ts
+++ apps/wallet/lib/hive.ts
@@ -2,7 +2,7 @@ import Big from 'big.js';
 import { AccountHistory } from '@/wallet/store/app-types';
 import { makeBitMaskFilter, operationOrders } from '@hiveio/dhive/lib/utils';
 import moment from 'moment';
-import { TWaxApiRequest, RcAccount } from '@hiveio/wax';
+import { TWaxApiRequest, RcAccount, asset } from '@hiveio/wax';
 import { hiveChainService } from '@transaction/lib/hive-chain-service';
 
 const chain = await hiveChainService.getHiveChain();
@@ -292,3 +292,24 @@ type GetRecentTradesyData = {
 export const getRecentTrades = async (limit: number = 1000): Promise<IRecentTradesData[]> => {
   return chain.extend<GetRecentTradesyData>().api.condenser_api.get_recent_trades([limit]);
 };
+
+type SavingsWithdrawals = {
+  withdrawals: {
+    amount: asset,
+    complete: Date,
+    from: string,
+    id: number,
+    memo: string,
+    request_id: number,
+    to: string
+  }[]
+}
+
+type GetSavingsWithdrawalsData = {
+  database_api: {
+    find_savings_withdrawals: TWaxApiRequest<{account: string}, SavingsWithdrawals>
+  }
+}
+export const getSavingsWithdrawals = async (account: string): Promise<SavingsWithdrawals> => {
+  return chain.extend<GetSavingsWithdrawalsData>().api.database_api.find_savings_withdrawals({account: account})
+}
