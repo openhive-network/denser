49629b4f84752124e31e1bbe4611eaa08494b847
Piotr Berezka <berezkapiotr@gmail.com>
2024-09-04 11:20:36 +0200

    add name and password generation for communities

--- apps/wallet/pages/[param]/communities.tsx
+++ apps/wallet/pages/[param]/communities.tsx
@@ -5,10 +5,42 @@ import WalletMenu from '@/wallet/components/wallet-menu';
 import { Button, Checkbox, Input, Label } from '@ui/components';
 import { useState } from 'react';
 import { getTranslations } from '../../lib/get-translations';
+import { createWaxFoundation } from '@hiveio/wax';
 
 function Communities({ username }: InferGetServerSidePropsType<typeof getServerSideProps>) {
   const { t } = useTranslation('common_wallet');
   const [nextStep, setNextStep] = useState(false);
+  const [generatedName, setGeneratedName] = useState<string>();
+  const [generatedPassword, setGeneratedPassword] = useState<string>();
+  const [publicKeys, setPublicKeys] = useState<{
+    active: string;
+    owner: string;
+    posting: string;
+  }>();
+
+  const handleNext = async () => {
+    setGeneratedName(`hive-${Math.floor(Math.random() * 100000) + 100000}`);
+
+    const wax = await createWaxFoundation();
+    // generate password
+    const brainKeyData = wax.suggestBrainKey();
+    const passwordToBeSavedByUser = brainKeyData.wifPrivateKey;
+    setGeneratedPassword(passwordToBeSavedByUser);
+
+    // private keys for account authorities
+    const owner = wax.getPrivateKeyFromPassword(username, 'owner', passwordToBeSavedByUser);
+    const active = wax.getPrivateKeyFromPassword(username, 'active', passwordToBeSavedByUser);
+    const posting = wax.getPrivateKeyFromPassword(username, 'posting', passwordToBeSavedByUser);
+
+    setPublicKeys({
+      active: active.associatedPublicKey,
+      owner: owner.associatedPublicKey,
+      posting: posting.associatedPublicKey
+    });
+
+    setNextStep(true);
+  };
+
   return (
     <ProfileLayout>
       <div className="flex flex-col gap-8 ">
@@ -26,13 +58,13 @@ function Communities({ username }: InferGetServerSidePropsType<typeof getServerS
             <Input />
           </div>
           {!nextStep ? (
-            <Button onClick={() => setNextStep(true)} className="w-fit">
+            <Button onClick={() => handleNext()} className="w-fit">
               Next
             </Button>
           ) : (
             <div className="flex flex-col gap-2">
               <div className="flex flex-col border-2 bg-white p-2 text-red-700">
-                <span>hive-219841u8941924</span> <span>sadinjasiudnaiusdnuwiawiodjaiowd</span>
+                <span>{generatedName}</span> <span>{generatedPassword}</span>
               </div>
               <div>
                 <div className="flex items-center space-x-2">
