55eed6e506a5dab166f9fe61bc0a38c584a59bc9
Krzysztof Kocot <k.kocot0@gmail.com>
2024-09-12 12:05:24 +0200

    better way to setup default prose settings

--- packages/tailwindcss/tailwind.config.js
+++ packages/tailwindcss/tailwind.config.js
@@ -80,6 +80,100 @@ module.exports = {
       animation: {
         'accordion-down': 'accordion-down 0.2s ease-out',
         'accordion-up': 'accordion-up 0.2s ease-out'
+      },
+      typography: {
+        DEFAULT: {
+          css: {
+            color: 'var(--primary)',
+            'h1, h2, h3, h4, h5, h6': {
+              color: 'var(--primary)',
+              marginBottom: '0.25rem',
+              marginTop: '2.5rem',
+              fontWeight: '600'
+            },
+            a: {
+              color: 'var(--destructive)',
+              textDecoration: 'none',
+              whiteSpace: 'pre-wrap',
+              wordBreak: 'break-word'
+            },
+            blockquote: {
+              color: 'var(--primary-70)',
+              margin: '0',
+              marginBottom: '1rem',
+              paddingLeft: '1.25rem',
+              paddingTop: '0.5rem',
+              fontWeight: '400',
+              textIndent: '-0.1875rem'
+            },
+            strong: {
+              color: 'var(--primary)'
+            },
+            code: {
+              backgroundColor: 'var(--background-secondary)',
+              color: 'var(--primary-70)',
+              fontFamily: 'Consolas, monospace',
+              fontSize: '14.4px',
+              padding: '0.1875rem',
+              wordBreak: 'break-word',
+              textIndent: '-0.1875rem',
+              lineHeight: '19px'
+            },
+            pre: {
+              backgroundColor: 'var(--background-secondary)',
+              margin: '0',
+              padding: '0',
+              textIndent: '-0.1875rem'
+            },
+            table: {
+              borderColor: 'var(--secondary)',
+              borderCollapse: 'collapse',
+              marginBottom: '1rem',
+              overflowX: 'auto',
+              maxWidth: '100%'
+            },
+            tr: {
+              backgroundColor: 'var(--background-secondary)',
+              '&:nth-child(even)': {
+                backgroundColor: 'var(--background)'
+              }
+            },
+            th: {
+              color: 'var(--primary)'
+            },
+            td: {
+              borderColor: 'var(--secondary)',
+              paddingLeft: '0.4rem',
+              paddingRight: '0.4rem',
+              paddingTop: '0.25rem',
+              paddingBottom: '0.25rem',
+              textAlign: 'middle',
+              fontSize: '16.3px'
+            },
+            img: {
+              marginBottom: '0.625rem',
+              marginTop: '0'
+            },
+            hr: {
+              marginTop: '1.25rem',
+              marginBottom: '1.25rem'
+            },
+            ol: {
+              marginBottom: '1rem',
+              marginLeft: '0.75rem',
+              marginTop: '0'
+            },
+            ul: {
+              marginBottom: '1rem',
+              marginLeft: '0.75rem',
+              marginTop: '0'
+            },
+            li: {
+              margin: '0',
+              padding: '0'
+            }
+          }
+        }
       }
     }
   },
