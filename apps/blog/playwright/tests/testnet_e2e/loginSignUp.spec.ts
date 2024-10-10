763469f5de746804d4d76cf49fb789208ec18085
Adam Kociubiński <akociubinski@syncad.com>
2024-10-10 12:54:22 +0200

    extend loginHelper class of login method

--- apps/blog/playwright/tests/testnet_e2e/loginSignUp.spec.ts
+++ apps/blog/playwright/tests/testnet_e2e/loginSignUp.spec.ts
@@ -1,14 +1,14 @@
 import { expect, test } from '@playwright/test';
 import { HomePage } from '../support/pages/homePage';
-import { LoginForm } from '../support/pages/loginForm';
-import { ProfileUserMenu } from '../support/pages/profileUserMenu';
-import { users } from '../support/loginHelper';
+import { LoginHelper, users } from '../support/loginHelper';
 
 test.describe('Login and Sign Up tests', () =>{
   let homePage: HomePage;
+  let loginHelper: LoginHelper;
 
   test.beforeEach(async ({ page }) => {
     homePage = new HomePage(page);
+    loginHelper = new LoginHelper(page);
 
     await homePage.goto();
   });
@@ -16,39 +16,19 @@ test.describe('Login and Sign Up tests', () =>{
   // -- Happy path for Login
   // Use denserautotest1 user
   test('Sign In to the Denser App', async ({page}) =>{
-    const loginFormDefaut = new LoginForm(page);
-    const profileMenu = new ProfileUserMenu(page);
-
-    await homePage.loginBtn.click()
-    await loginFormDefaut.validateDefaultLoginFormIsLoaded();
-    // Sign In
-    await loginFormDefaut.usernameInput.fill(users.denserautotest1.username);
-    await loginFormDefaut.passwordInput.fill(users.denserautotest1.safeStoragePassword);
-    await loginFormDefaut.wifInput.fill(users.denserautotest1.keys.private_posting); // Posting Key
-    await loginFormDefaut.saveSignInButton.click();
-    await homePage.profileAvatarButton.click();
-    // Validate User is logged in
-    await page.waitForSelector(profileMenu.profileMenuContent['_selector']);
-    await profileMenu.validateUserProfileManuIsOpen();
-    await profileMenu.validateUserNameInProfileMenu(users.denserautotest1.username);
+    await loginHelper.login(
+      users.denserautotest1.username,
+      users.denserautotest1.safeStoragePassword,
+      users.denserautotest1.keys.private_posting
+    );
   });
 
   // Use denserautotest2 user
   test('Sign In to the Denser App by another user', async ({page}) =>{
-    const loginFormDefaut = new LoginForm(page);
-    const profileMenu = new ProfileUserMenu(page);
-
-    await homePage.loginBtn.click()
-    await loginFormDefaut.validateDefaultLoginFormIsLoaded();
-    // Sign In
-    await loginFormDefaut.usernameInput.fill(users.denserautotest2.username);
-    await loginFormDefaut.passwordInput.fill(users.denserautotest2.safeStoragePassword);
-    await loginFormDefaut.wifInput.fill(users.denserautotest2.keys.private_posting); // Posting Key
-    await loginFormDefaut.saveSignInButton.click();
-    await homePage.profileAvatarButton.click();
-    // Validate User is logged in
-    await page.waitForSelector(profileMenu.profileMenuContent['_selector']);
-    await profileMenu.validateUserProfileManuIsOpen();
-    await profileMenu.validateUserNameInProfileMenu(users.denserautotest2.username);
+    await loginHelper.login(
+      users.denserautotest2.username,
+      users.denserautotest2.safeStoragePassword,
+      users.denserautotest2.keys.private_posting
+    );
   });
 });
