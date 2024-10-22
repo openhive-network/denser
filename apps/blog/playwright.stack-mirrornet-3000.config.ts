0c880880e9ab8690721b1ac1583df8c3c31d085a
Adam Kociubiński <akociubinski@syncad.com>
2024-10-22 08:55:32 +0200

    add setup dependencies to firefox and webkit projects

--- apps/blog/playwright.stack-mirrornet-3000.config.ts
+++ apps/blog/playwright.stack-mirrornet-3000.config.ts
@@ -65,12 +65,14 @@ export default defineConfig({
 
     {
       name: 'firefox',
-      use: { ...devices['Desktop Firefox'] }
+      use: { ...devices['Desktop Firefox'] },
+      dependencies: ['setup']
     },
 
     {
       name: 'webkit',
-      use: { ...devices['Desktop Safari'] }
+      use: { ...devices['Desktop Safari'] },
+      dependencies: ['setup']
     }
 
     /* Test against mobile viewports. */
