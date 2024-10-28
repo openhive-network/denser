454661383ba83f2a8ad36e9e3a7d5b4ede468271
Adam Kociubiński <akociubinski@syncad.com>
2024-10-28 09:25:26 +0100

    add tests for downvotes

--- apps/blog/playwright/tests/testnet_e2e/votingPOM.spec.ts
+++ apps/blog/playwright/tests/testnet_e2e/votingPOM.spec.ts
@@ -13,7 +13,7 @@ test.describe('Voting tests with fixture and POM', () =>{
     await homePage.goto();
   });
 
-  test.describe.serial('Upvote group', () => {
+  test.describe('Upvote group', () => {
     test('Upvote the first post of the tranding list', async ({denserAutoTest4Page}) =>{
       const loginForm = new LoginForm(denserAutoTest4Page.page);
       const loginHelper = new LoginHelper(denserAutoTest4Page.page);
@@ -94,6 +94,91 @@ test.describe('Voting tests with fixture and POM', () =>{
         await firstPostUpvoteButtonLocator.waitFor({state: 'visible'});
         expect(await homePage.getElementCssPropertyValue(firstPostUpvoteButtonLocator, 'color')).toBe('rgb(255, 255, 255)');
         expect(await homePage.getElementCssPropertyValue(firstPostUpvoteButtonLocator, 'background-color')).toBe('rgb(255, 0, 0)');
+      }
+    });
+  });
+
+  test.describe('Downvote group', () => {
+    test('Downvote the second post of the tranding list', async ({denserAutoTest4Page}) =>{
+      const loginForm = new LoginForm(denserAutoTest4Page.page);
+      const loginHelper = new LoginHelper(denserAutoTest4Page.page);
+
+      // Validate User is logged in as denserautotest4
+      await loginHelper.validateLoggedInUser(users.denserautotest4.username);
+      // Click to close the profile menu
+      await denserAutoTest4Page.page.getByTestId('community-name').locator('..').locator('..').click({force: true});
+      // Validate that Downvote button of the first color
+      const secondPostDownvoteButtonLocator = denserAutoTest4Page.page.getByTestId('post-list-item').nth(1).getByTestId('downvote-button').locator('svg');
+      const secondPostDownvoteButtonLocatorToClick = denserAutoTest4Page.page.getByTestId('post-list-item').nth(1).getByTestId('downvote-button');
+      if (await homePage.getElementCssPropertyValue(secondPostDownvoteButtonLocator, 'color') == 'rgb(75, 85, 99)'){
+        // Validate that Upvote button of the first color red
+        expect(await homePage.getElementCssPropertyValue(secondPostDownvoteButtonLocator, 'color')).toBe('rgb(75, 85, 99)');
+        expect(await homePage.getElementCssPropertyValue(secondPostDownvoteButtonLocator, 'background-color')).toBe('rgba(0, 0, 0, 0)');
+      } else {
+        // Wait until optimistic ui is finished and validate the color of the downvote button
+        await secondPostDownvoteButtonLocator.waitFor({state: 'visible'});
+        expect(await homePage.getElementCssPropertyValue(secondPostDownvoteButtonLocator, 'color')).toBe('rgb(255, 255, 255)');
+        expect(await homePage.getElementCssPropertyValue(secondPostDownvoteButtonLocator, 'background-color')).toBe('rgb(255, 0, 0)');
+      }
+      // Click Downvote button of the second post on the trending list
+      await secondPostDownvoteButtonLocatorToClick.click();
+      // If a password to unlock key is needed
+      await loginForm.page.waitForTimeout(2000);
+      await loginForm.putEnterYourPasswordToUnlockKeyIfNeeded(users.denserautotest4.safeStoragePassword);
+      await secondPostDownvoteButtonLocator.waitFor({state: 'visible'});
+      await loginForm.page.waitForTimeout(2000);
+      if (await homePage.getElementCssPropertyValue(secondPostDownvoteButtonLocator, 'color') == 'rgb(255, 255, 255)') {
+        // Wait until optimistic ui is finished and validate the color of the upvote button
+        expect(await homePage.getElementCssPropertyValue(secondPostDownvoteButtonLocator, 'color')).toBe('rgb(255, 255, 255)');
+        expect(await homePage.getElementCssPropertyValue(secondPostDownvoteButtonLocator, 'background-color')).toBe('rgb(255, 0, 0)');
+      } else {
+        // Validate that Downvote button of the second post has color grey
+        expect(await homePage.getElementCssPropertyValue(secondPostDownvoteButtonLocator, 'color')).toBe('rgb(75, 85, 99)');
+        expect(await homePage.getElementCssPropertyValue(secondPostDownvoteButtonLocator, 'background-color')).toBe('rgba(0, 0, 0, 0)');
+      }
+    });
+
+    test('Downvote the second post of the tranding list again', async ({denserAutoTest4Page}) =>{
+      const loginForm = new LoginForm(denserAutoTest4Page.page);
+      const loginHelper = new LoginHelper(denserAutoTest4Page.page);
+      const profileMenu = new ProfileUserMenu(denserAutoTest4Page.page);
+
+      // Validate User is logged in as denserautotest4
+      await loginHelper.validateLoggedInUser(users.denserautotest4.username);
+      // Click to close the profile menu
+      await denserAutoTest4Page.page.getByTestId('community-name').locator('..').locator('..').click({force: true});
+      // Get the second downvote button locator
+      const secondPostDownvoteButtonLocator = denserAutoTest4Page.page.getByTestId('post-list-item').nth(1).getByTestId('downvote-button').locator('svg');
+      const secondPostDownvoteButtonLocatorToClick = denserAutoTest4Page.page.getByTestId('post-list-item').nth(1).getByTestId('downvote-button');
+
+      if (await homePage.getElementCssPropertyValue(secondPostDownvoteButtonLocator, 'color') == 'rgb(255, 255, 255)') {
+        // Wait until optimistic ui is finished and validate the color of the downvote button
+        expect(await homePage.getElementCssPropertyValue(secondPostDownvoteButtonLocator, 'color')).toBe('rgb(255, 255, 255)');
+        expect(await homePage.getElementCssPropertyValue(secondPostDownvoteButtonLocator, 'background-color')).toBe('rgb(255, 0, 0)');
+      } else {
+        // Validate that Downvote button of the second post is color grey
+        expect(await homePage.getElementCssPropertyValue(secondPostDownvoteButtonLocator, 'color')).toBe('rgb(75, 85, 99)');
+        expect(await homePage.getElementCssPropertyValue(secondPostDownvoteButtonLocator, 'background-color')).toBe('rgba(0, 0, 0, 0)');
+      }
+      // Click Downvote button of the second post on the trending list
+      await secondPostDownvoteButtonLocatorToClick.click({force: true});
+      // If a password to unlock key is needed
+      await loginForm.page.waitForTimeout(2000);
+      await loginForm.putEnterYourPasswordToUnlockKeyIfNeeded(users.denserautotest4.safeStoragePassword);
+      // Wait until optimistic ui is finished and validate the color of the downvote button
+      await secondPostDownvoteButtonLocator.waitFor({state: 'visible'});
+      // Move pointer from the upvote icon
+      await denserAutoTest4Page.page.getByTestId('community-name').locator('..').locator('..').click({force: true});
+
+      if (await homePage.getElementCssPropertyValue(secondPostDownvoteButtonLocator, 'color') == 'rgb(75, 85, 99)'){
+        // Validate that Downvote button of the second post is color grey
+        expect(await homePage.getElementCssPropertyValue(secondPostDownvoteButtonLocator, 'color')).toBe('rgb(75, 85, 99)');
+        expect(await homePage.getElementCssPropertyValue(secondPostDownvoteButtonLocator, 'background-color')).toBe('rgba(0, 0, 0, 0)');
+      } else {
+        // Wait until optimistic ui is finished and validate the color of the downvote button
+        await secondPostDownvoteButtonLocator.waitFor({state: 'visible'});
+        expect(await homePage.getElementCssPropertyValue(secondPostDownvoteButtonLocator, 'color')).toBe('rgb(255, 255, 255)');
+        expect(await homePage.getElementCssPropertyValue(secondPostDownvoteButtonLocator, 'background-color')).toBe('rgb(255, 0, 0)');
       }
     });
   });
