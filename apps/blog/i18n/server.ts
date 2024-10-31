017784125f5a3c785f2bc15040a36927fa5bf2d0
Krzysztof Kocot <k.kocot0@gmail.com>
2024-10-31 13:48:01 +0100

    setup i18n in client components

---
+++ apps/blog/i18n/server.ts
@@ -0,0 +1,27 @@
+import { createInstance, InitOptions } from 'i18next';
+import resourcesToBackend from 'i18next-resources-to-backend';
+import { initReactI18next } from 'react-i18next/initReactI18next';
+import { getOptions, cookieName, defaultLocale } from './settings';
+import { cookies } from 'next/headers';
+
+const initI18next = async (lng: string, ns: string) => {
+  const i18nInstance = createInstance();
+  await i18nInstance
+    .use(initReactI18next)
+    .use(
+      resourcesToBackend(
+        (language: string, namespace: string) => import(`../locales/${language}/${namespace}.json`)
+      )
+    )
+    .init(getOptions(lng, ns));
+  return i18nInstance;
+};
+
+export async function useTranslation(ns: string, options: any = {}) {
+  const lng = cookies().get(cookieName)?.value ?? defaultLocale;
+  const i18nextInstance = await initI18next(lng, ns);
+  return {
+    t: i18nextInstance.getFixedT(lng, Array.isArray(ns) ? ns[0] : ns, options.keyPrefix),
+    i18n: i18nextInstance
+  };
+}
