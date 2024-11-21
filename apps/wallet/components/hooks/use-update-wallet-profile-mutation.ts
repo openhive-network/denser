bc6c576996caad43cc137b8d50dddde4c7709087
Krzysztof Kocot <k.kocot0@gmail.com>
2024-11-21 14:18:23 +0100

    setup use mutation hook

--- apps/wallet/components/hooks/use-update-wallet-profile-mutation.ts
+++ apps/wallet/components/hooks/use-update-wallet-profile-mutation.ts
@@ -2,6 +2,8 @@ import { useUser } from '@smart-signer/lib/auth/use-user';
 import { useMutation, useQueryClient } from '@tanstack/react-query';
 import { Authorizes, transactionService } from '@transaction/index';
 import { getLogger } from '@ui/lib/logging';
+import { Authority } from '@hiveio/dhive/lib/chain/account';
+
 const logger = getLogger('app');
 
 /**
@@ -17,17 +19,27 @@ export function useUpdateProfileMutation() {
     mutationFn: async (params: {
       memo_key: string;
       json_metadata: string;
-      owner: Authorizes;
-      active: Authorizes;
-      posting: Authorizes;
+      owner: Authority | undefined;
+      active: Authority | undefined;
+      posting: Authority | undefined;
     }) => {
       const { memo_key, json_metadata, owner, active, posting } = params;
+      const transformAuthority = (authority: Authority | undefined): Authorizes | undefined => {
+        if (!authority) return undefined;
+
+        return {
+          ...authority,
+          account_auths: Object.fromEntries(authority.account_auths),
+          key_auths: Object.fromEntries(authority.key_auths.map(([key, value]) => [key.toString(), value]))
+        };
+      };
+
       const broadcastResult = await transactionService.updateWalletProfile(
         memo_key,
         json_metadata,
-        owner,
-        active,
-        posting,
+        transformAuthority(owner),
+        transformAuthority(active),
+        transformAuthority(posting),
         { observe: true }
       );
       const response = { ...params, broadcastResult };
