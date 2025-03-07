191063de23013b595a7496d5a153481f6fbdf713
Adam Kociubiński <akociubinski@syncad.com>
2025-03-07 10:55:40 +0100

    improve description in wallet login dialog

--- apps/wallet/playwright/tests/support/pages/loginForm.ts
+++ apps/wallet/playwright/tests/support/pages/loginForm.ts
@@ -84,7 +84,7 @@ export class LoginForm {
 
   async validateDefaultLoginFormIsLoaded() {
     await this.page.waitForSelector(this.loginFormDescription['_selector']);
-    await expect(this.loginFormDescription).toHaveText('Save your posting key by filling form below');
+    await expect(this.loginFormDescription).toHaveText('Save your active key by filling form below');
     await expect(this.usernameInput).toHaveAttribute('placeholder', 'Username');
     await expect(this.passwordInput).toHaveAttribute('placeholder', 'Safe storage password');
     await expect(this.wifInput).toHaveAttribute('placeholder', 'WIF posting private key');
