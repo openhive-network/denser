b288494a4831b556fc86a169fb6e4cfde4f005cd
Krzysztof Kocot <k.kocot0@gmail.com>
2025-03-12 20:35:42 +0100

    Change order of plugins import

--- packages/renderer/src/index.ts
+++ packages/renderer/src/index.ts
@@ -1,14 +1,14 @@
 import {DefaultRenderer} from './renderers/default/DefaultRenderer';
-import {TablePlugin} from './renderers/default/plugins/TablePlugin';
 import {InstagramPlugin} from './renderers/default/plugins/InstagramPlugin';
+import {TablePlugin} from './renderers/default/plugins/TablePlugin';
 import {SpoilerPlugin} from './renderers/default/plugins/SpoilerPlugin';
 import {TwitterPlugin} from './renderers/default/plugins/TwitterPlugin';
 
 export {DefaultRenderer} from './renderers/default/DefaultRenderer';
 export {SpoilerPlugin} from './renderers/default/plugins/SpoilerPlugin';
-export {TablePlugin} from './renderers/default/plugins/TablePlugin';
 export {TwitterPlugin} from './renderers/default/plugins/TwitterPlugin';
 export {InstagramPlugin} from './renderers/default/plugins/InstagramPlugin';
+export {TablePlugin} from './renderers/default/plugins/TablePlugin';
 export type {RendererPlugin} from './renderers/default/plugins/RendererPlugin';
 
 export const HiveContentRenderer = {
