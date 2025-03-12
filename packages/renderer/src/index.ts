efa0321b4cb05c1a38e85c1e87b272552f9dc010
Krzysztof Kocot <k.kocot0@gmail.com>
2025-03-12 14:19:24 +0100

    Create Plugin in renderer to wrap tables with div

--- packages/renderer/src/index.ts
+++ packages/renderer/src/index.ts
@@ -2,18 +2,21 @@ import {DefaultRenderer} from './renderers/default/DefaultRenderer';
 import {InstagramPlugin} from './renderers/default/plugins/InstagramPlugin';
 import {SpoilerPlugin} from './renderers/default/plugins/SpoilerPlugin';
 import {TwitterPlugin} from './renderers/default/plugins/TwitterPlugin';
+import {TablePlugin} from './renderers/default/plugins/TablePlugin';
 
 export {DefaultRenderer} from './renderers/default/DefaultRenderer';
 export {SpoilerPlugin} from './renderers/default/plugins/SpoilerPlugin';
 export {TwitterPlugin} from './renderers/default/plugins/TwitterPlugin';
 export {InstagramPlugin} from './renderers/default/plugins/InstagramPlugin';
+export {TablePlugin} from './renderers/default/plugins/TablePlugin';
 export type {RendererPlugin} from './renderers/default/plugins/RendererPlugin';
 
 export const HiveContentRenderer = {
     DefaultRenderer,
     SpoilerPlugin,
     TwitterPlugin,
-    InstagramPlugin
+    InstagramPlugin,
+    TablePlugin
 };
 
 export default HiveContentRenderer;
