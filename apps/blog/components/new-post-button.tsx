136cb6f6966e382c721f713985c1a60696d40c29
Krzysztof Kocot <k.kocot0@gmail.com>
2025-04-08 13:48:21 +0200

    Use full defaultValues to save post information in local storage

--- apps/blog/components/new-post-button.tsx
+++ apps/blog/components/new-post-button.tsx
@@ -2,6 +2,8 @@ import { useLocalStorage } from 'usehooks-ts';
 import { Button } from '@ui/components';
 import { useTranslation } from 'next-i18next';
 import Link from 'next/link';
+import { DEFAULT_PREFERENCES, Preferences } from '../pages/[param]/settings';
+import { useUser } from '@smart-signer/lib/auth/use-user';
 
 type AccountFormValues = {
   title: string;
@@ -11,16 +13,25 @@ type AccountFormValues = {
   author: string;
   category: string;
 };
-const defaultValues = {
-  title: '',
-  postArea: '',
-  postSummary: '',
-  tags: '',
-  author: '',
-  category: ''
-};
 const NewPost = ({ name, disabled }: { name: string; disabled: boolean }) => {
+  const { user } = useUser();
   const { t } = useTranslation('common_blog');
+  const [preferences] = useLocalStorage<Preferences>(
+    `user-preferences-${user.username}`,
+    DEFAULT_PREFERENCES
+  );
+
+  const defaultValues = {
+    title: '',
+    postArea: '',
+    postSummary: '',
+    tags: '',
+    author: '',
+    category: 'blog',
+    beneficiaries: [],
+    maxAcceptedPayout: preferences.blog_rewards === '0%' ? 0 : 1000000,
+    payoutType: preferences.blog_rewards
+  };
   const [storedPost, storePost] = useLocalStorage<AccountFormValues>('postData-new', defaultValues);
   return (
     <Button
