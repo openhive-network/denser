10212da3f158f23170ed9486e83de0eb424b841f
Bartek Wrona <wrona@syncad.com>
2025-05-22 00:00:23 +0200

    run_instance.sh allows to specify env-file to override deployment variables. Also removed overriding of TURBO_APP_PATH and TURBO_APP_NAME which are already specified by built image

--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -2,6 +2,12 @@
 
 set -e
 
+# point default .env file
+ln -sf "${APP_ENV_FILE_PATH}" /app${TURBO_APP_PATH}/.env
+
+echo "Contents of /app${TURBO_APP_PATH}/.env"
+cat "/app${TURBO_APP_PATH}/.env"
+
 echo "Setting environment..."
 DIR=$(pwd)
 cd ".${TURBO_APP_PATH}"
