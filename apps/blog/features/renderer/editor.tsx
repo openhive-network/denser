1c9eb3acf289c0f53f9ac9eb11baa99394924298
Krzysztof Kocot <k.kocot0@gmail.com>
2025-06-09 14:36:54 +0200

    Create Editor component

---
+++ apps/blog/features/renderer/editor.tsx
@@ -0,0 +1,20 @@
+import { MdEditor } from 'md-editor-rt';
+import 'md-editor-rt/lib/style.css';
+import { useTheme } from 'next-themes';
+
+const DenserMdEditor = ({ text, onChange }: { text: string; onChange: (text: string) => void }) => {
+  const { resolvedTheme } = useTheme();
+
+  return (
+    <MdEditor
+      language="en-US"
+      value={text}
+      onChange={onChange}
+      preview={false}
+      theme={resolvedTheme === 'dark' ? 'dark' : 'light'}
+      toolbarsExclude={['github', 'htmlPreview', 'previewOnly', 'preview', 'save', 'mermaid']}
+    />
+  );
+};
+
+export default DenserMdEditor;
