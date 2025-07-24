ead8c5b0526e046c57a31c3cfa0f5382e7384d5d
Krzysztof Kocot <k.kocot0@gmail.com>
2025-07-24 11:23:55 +0200

    Optimistic ui on Claim Rewards

--- apps/blog/components/hooks/use-claim-reward-mutation.ts
+++ apps/blog/components/hooks/use-claim-reward-mutation.ts
@@ -3,6 +3,7 @@ import { useMutation, useQueryClient } from '@tanstack/react-query';
 import { transactionService } from '@transaction/index';
 import { useUser } from '@smart-signer/lib/auth/use-user';
 import { getLogger } from '@ui/lib/logging';
+import { FullAccount } from '@transaction/lib/app-types';
 const logger = getLogger('app');
 
 /**
@@ -14,20 +15,35 @@ const logger = getLogger('app');
 export function useClaimRewardsMutation() {
   const queryClient = useQueryClient();
   const { user } = useUser();
+  const queryKey = ['profileData', user.username];
   const claimRewardMutation = useMutation({
     mutationFn: async (params: { account: ApiAccount }) => {
       const { account } = params;
 
       const broadcstResult = await transactionService.claimRewards(account, { observe: true });
-      const response = { ...params, broadcstResult };
+      const prevData: FullAccount | undefined = queryClient.getQueryData(queryKey);
+      const response = { ...params, broadcstResult, prevData };
 
       logger.info('Done claim reward tranasaction: %o', response);
       return response;
     },
+    onSettled: (data) => {
+      if (!data) return;
+      const { prevData } = data;
+      if (prevData) {
+        queryClient.setQueryData(queryKey, () => ({
+          ...prevData,
+          reward_hbd_balance: '0.000 HBD',
+          reward_hive_balance: '0.000 HIVE',
+          reward_vesting_hive: '0.000 HIVE'
+        }));
+      }
+    },
     onSuccess: (data) => {
       logger.info('useClaimRewardMutation onSuccess data: %o', data);
-      const { username } = user;
-      queryClient.invalidateQueries({ queryKey: ['profileData', username] });
+      setTimeout(() => {
+        queryClient.invalidateQueries({ queryKey });
+      }, 3000);
     }
   });
 
