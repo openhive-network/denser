8111433324bb91043aa3f5a9023775642decedf6
Krzysztof Kocot <k.kocot0@gmail.com>
2025-08-11 13:30:08 +0200

    Fix #684, add button to hide low rated suggested posts again

--- apps/blog/components/suggestions-list.tsx
+++ apps/blog/components/suggestions-list.tsx
@@ -4,7 +4,6 @@ import SuggestionsCard from './suggestions-card';
 import { Button } from '@ui/components';
 
 // FIXME: This is a temporary fix to avoid the error when suggestions is not an array
-// FIXME: Source of data should use Wax not direct hivesense API call via fetch
 const SuggestionsList = ({ suggestions }: { suggestions: Entry[] }) => {
   const [filteredSuggestions, setFilteredSuggestions] = useState<Entry[]>(
     Array.isArray(suggestions) ? suggestions.filter((e) => e.author_reputation >= 50 && !e.stats?.gray) : []
@@ -16,21 +15,26 @@ const SuggestionsList = ({ suggestions }: { suggestions: Entry[] }) => {
   }
 
   return (
-    <div className="flex md:flex-col">
+    <div className="my-4 flex md:flex-col">
       {filteredSuggestions.length > 0 ? (
         filteredSuggestions.map((suggestion, i) => <SuggestionsCard entry={suggestion} key={i} />)
       ) : (
         <div className="flex flex-col items-center gap-2 p-4 text-sm">
           <p>Sorry</p>
           <p>All suggested posts were hidden due to low ratings.</p>
-          <Button
-            onClick={() => setFilteredSuggestions(Array.isArray(suggestions) ? suggestions : [])}
-            variant="outlineRed"
-          >
-            Show
-          </Button>
         </div>
       )}
+      <Button
+        className="w-1/2 self-center"
+        onClick={() =>
+          filteredSuggestions.length === suggestions.length
+            ? setFilteredSuggestions(suggestions.filter((e) => e.author_reputation >= 50 && !e.stats?.gray))
+            : setFilteredSuggestions(suggestions)
+        }
+        variant="outlineRed"
+      >
+        {filteredSuggestions.length === suggestions.length ? 'Hide' : 'Show All'}
+      </Button>
     </div>
   );
 };
