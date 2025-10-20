606837c73125f7cb5c201f66e56d13edc9129eda
BardDev <k.kocot0@gmail.com>
2025-10-20 08:12:11 +0200

    Create posts tabs layout

--- apps/blog/app/[param]/(user-profile)/(posts-tabs)/layout.tsx
+++ apps/blog/app/[param]/(user-profile)/(posts-tabs)/layout.tsx
@@ -1,13 +1,7 @@
-const Layout = ({ children }: { children: React.ReactNode }) => {
-  return (
-    <div>
-      <ul>
-        <li>Posts</li>
-        <li>Comments</li>
-        <li>Payout</li>
-      </ul>
-      {children}
-    </div>
-  );
+import AccountPostsTabs from '@/blog/features/layouts/account-posts/tabs';
+import { ReactNode } from 'react';
+
+const Layout = async ({ children, params }: { children: ReactNode; params: { param: string } }) => {
+  return <AccountPostsTabs username={params.param.replace('%40', '')}>{children}</AccountPostsTabs>;
 };
 export default Layout;
