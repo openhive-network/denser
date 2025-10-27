db4ecaa3b265e125f39734dfd019d03fbf057bb3
Krzysztof Kocot <k.kocot0@gmail.com>
2025-10-27 07:59:50 +0100

    Fix browser console errors

--- apps/blog/features/post-rendering/flag-icon.tsx
+++ apps/blog/features/post-rendering/flag-icon.tsx
@@ -1,17 +1,20 @@
+import React from 'react';
 import { Icons } from '@ui/components/icons';
 import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from '@ui/components/tooltip';
 
-const FlagTooltip = ({ onClick }: { onClick: () => void }) => {
+const FlagTooltip = React.forwardRef<HTMLButtonElement, { onClick: () => void }>(({ onClick }, ref) => {
   return (
     <TooltipProvider>
       <Tooltip>
-        <TooltipTrigger>
+        <TooltipTrigger ref={ref}>
           <Icons.flag className="h-4" onClick={onClick} />
         </TooltipTrigger>
         <TooltipContent>Flag post</TooltipContent>
       </Tooltip>
     </TooltipProvider>
   );
-};
+});
+
+FlagTooltip.displayName = 'FlagTooltip';
 
 export default FlagTooltip;
--- packages/ui/components/loading.tsx
+++ packages/ui/components/loading.tsx
@@ -1,12 +1,14 @@
 'use client';
 
 import clsx from 'clsx';
-import { TraceSpinner } from 'react-spinners-kit';
+import { Icons } from './icons';
 
 const Loading = ({ loading, className }: { loading: boolean; className?: string }) => {
+  if (!loading) return null;
+
   return (
     <div className={clsx('flex h-full w-full items-center justify-center pt-16', className)}>
-      <TraceSpinner size={50} frontColor="#dc2626" backColor="#f67173" loading={loading} />
+      <Icons.spinner className="h-12 w-12 animate-spin text-red-600" />
     </div>
   );
 };
