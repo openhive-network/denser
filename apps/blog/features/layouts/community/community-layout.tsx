57950c6fac4a7c093cbc4058042de1010ad50b6c
jlachor <jlachor@syncad.com>
2025-11-20 12:46:00 +0100

    Fix typo on the community page

--- apps/blog/features/layouts/community/community-layout.tsx
+++ apps/blog/features/layouts/community/community-layout.tsx
@@ -51,7 +51,6 @@ const CommunityLayout = ({ children, community }: { children: ReactNode; communi
       <div className="grid grid-cols-12 md:gap-4">
         <div className="hidden md:col-span-3 md:flex xl:col-span-2">
           {!!mySubsData ? <CommunitiesMyBar data={mySubsData} /> : <CommunitiesSidebar />}
-          <div>TEST COMMUNITY LAY</div>
         </div>
         <div className="col-span-12 md:col-span-9 xl:col-span-8">
           <div data-testid="card-explore-hive-mobile" className="md:col-span-10 md:flex xl:hidden">
