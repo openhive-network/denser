df2afeb0b4ba0051d6687f14d73cfc726c559ce3
jlachor <jlachor@syncad.com>
2025-11-24 14:37:02 +0100

    Get the server rendering back on my page

--- apps/blog/app/(main-and-community)/muted/my/page.tsx
+++ apps/blog/app/(main-and-community)/muted/my/page.tsx
@@ -1,10 +1,13 @@
+import SortPage from '@/blog/features/tags-pages/sort-page';
 import Content from './content';
 
 const sort = 'muted';
 const tag = 'my';
 
 const Page = () => (
-  <Content />
+  <SortPage sort={sort} tag={tag}>
+    <Content />
+  </SortPage>
 );
 
 export default Page;
--- apps/blog/app/(main-and-community)/trending/my/page.tsx
+++ apps/blog/app/(main-and-community)/trending/my/page.tsx
@@ -1,10 +1,13 @@
+import SortPage from '@/blog/features/tags-pages/sort-page';
 import Content from './content';
 
 const sort = 'trending';
 const tag = 'my';
 
 const Page = () => (
+  <SortPage sort={sort} tag={tag}>
     <Content />
+  </SortPage>
 );
 
 export default Page;
