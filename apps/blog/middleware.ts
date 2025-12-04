e4baf7047a31cc323457157db8eedb116b2ec9c3
Krzysztof Kocot <k.kocot0@gmail.com>
2025-12-04 15:13:28 +0100

    Add missing setLoginChallengeCookies to fix visit site console logs

--- apps/blog/middleware.ts
+++ apps/blog/middleware.ts
@@ -1,13 +1,19 @@
 import { type NextRequest, NextResponse } from 'next/server';
+import { setLoginChallengeCookies } from '@hive/smart-signer/lib/middleware-challenge-cookies';
 
 export async function middleware(request: NextRequest): Promise<NextResponse> {
   const { pathname } = request.nextUrl;
   const basePath = process.env.NEXT_PUBLIC_BASE_PATH || '';
 
-  /// In blog, let's redirect root path to /trending
+  const res = NextResponse.next();
+
+  // Set login challenge cookies (needed for console logging process)
+  setLoginChallengeCookies(request, res);
+
+  // In blog, redirect root path to /trending
   if (pathname === '/' || pathname === `${basePath}` || pathname === `${basePath}/`) {
     return NextResponse.redirect(new URL(`${basePath}/trending`, request.url), { status: 302 });
   }
 
-  return NextResponse.next();
+  return res;
 }
