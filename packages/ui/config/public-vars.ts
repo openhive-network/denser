7cd342868899c03ea1b6d533ac5f22191bd55436
jlachor <jlachor@syncad.com>
2025-12-05 14:50:03 +0100

    Correct env variable for better HC

--- packages/ui/config/public-vars.ts
+++ packages/ui/config/public-vars.ts
@@ -5,6 +5,6 @@ import env from '@beam-australia/react-env';
 export const configuredAIDomain = env('AI_DOMAIN') ?? 'https://api.syncad.com';
 export const configuredSiteDomain = env('SITE_DOMAIN') ?? 'https://hive.blog/';
 export const configuredImagesEndpoint = env('IMAGES_ENDPOINT') ?? 'https://images.hive.blog/';
-export const configuredApiEndpoint = env('API_ENDPOINT') ?? 'https://api.hive.blog/';
+export const configuredApiEndpoint = env('API_ENDPOINT') ?? 'https://api.hive.blog';
 export const configuredBlogDomain = env('BLOG_DOMAIN') ?? 'https://hive.blog/';
 export const configuredSessionTime = env('APP_SESSION_TIME') ?? configuredSiteDomain.includes('wallet') ? 900 : 64800; 
