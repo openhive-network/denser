b794e7f4ab18c99c73dffa14fc3b87e464cb3733
Gandalf <gandalf@storm.pl>
2026-02-15 12:42:23 +0100

    Fix TypeScript error: add rewardOptionsChanged to onMutate params

--- apps/blog/features/post-editor/hooks/use-post-mutation.ts
+++ apps/blog/features/post-editor/hooks/use-post-mutation.ts
@@ -38,6 +38,7 @@ export function usePostMutation() {
       beneficiaries: Beneficiarie[];
       maxAcceptedPayout: NaiAsset;
       percentHbd: number;
+      rewardOptionsChanged?: boolean;
     }) => {
       const {
         permlink,
